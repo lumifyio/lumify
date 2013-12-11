562c017b9951cf2a885df15be4e694a0cfcea2fd
Joe Ferner <joe@fernsroth.com>
2013-12-11 13:42:51 -0500

    fix KafkaWorkQueueRepository to work with multiple zk servers

--- lumify-model-kafka/src/main/java/com/altamiracorp/lumify/model/KafkaWorkQueueRepository.java
+++ lumify-model-kafka/src/main/java/com/altamiracorp/lumify/model/KafkaWorkQueueRepository.java
@@ -8,6 +8,7 @@ import kafka.producer.ProducerConfig;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.Properties;
 
@@ -18,12 +19,33 @@ public class KafkaWorkQueueRepository extends WorkQueueRepository {
     public void init(Map config) {
         super.init(config);
 
-        String zookeeperServerNames = (String) config.get(Configuration.ZK_SERVERS);
+        String zkServerNames = getZkConnectString(config.get(Configuration.ZK_SERVERS));
         Properties props = new Properties();
-        props.put("zk.connect", zookeeperServerNames + "/kafka"); // TODO what happens if zookeeperServerNames has multiple names
+        props.put("zk.connect", zkServerNames);
         props.put("serializer.class", KafkaJsonEncoder.class.getName());
         ProducerConfig producerConfig = new ProducerConfig(props);
         kafkaProducer = new Producer<String, JSONObject>(producerConfig);
+    }
+
+    private String getZkConnectString(Object zkServers) {
+        String zkServerNames;
+        if (zkServers instanceof Collection) {
+            Collection c = (Collection) zkServers;
+            StringBuilder temp = new StringBuilder();
+            boolean first = true;
+            for (Object zkServer : c) {
+                if (!first) {
+                    temp.append(",");
+                }
+                temp.append(zkServer.toString());
+                temp.append("/kafka");
+                first = false;
+            }
+            zkServerNames = temp.toString();
+        } else {
+            zkServerNames = zkServers + "/kafka";
+        }
+        return zkServerNames;
     }
 
     @Override
