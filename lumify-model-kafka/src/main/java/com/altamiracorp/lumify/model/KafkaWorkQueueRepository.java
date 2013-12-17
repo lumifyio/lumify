b6909ceb9279e952ae815ba0e7beb26cd7215e1a
Joe Ferner <joe@fernsroth.com>
2013-12-17 13:44:47 -0500

    refactored storm runner to have a commons base class and made some of the repos singletons

--- lumify-model-kafka/src/main/java/com/altamiracorp/lumify/model/KafkaWorkQueueRepository.java
+++ lumify-model-kafka/src/main/java/com/altamiracorp/lumify/model/KafkaWorkQueueRepository.java
@@ -2,6 +2,7 @@ package com.altamiracorp.lumify.model;
 
 import com.altamiracorp.lumify.core.config.Configuration;
 import com.altamiracorp.lumify.core.model.workQueue.WorkQueueRepository;
+import com.google.inject.Singleton;
 import kafka.javaapi.producer.Producer;
 import kafka.javaapi.producer.ProducerData;
 import kafka.producer.ProducerConfig;
@@ -13,25 +14,31 @@ import org.slf4j.LoggerFactory;
 import java.util.Map;
 import java.util.Properties;
 
+@Singleton
 public class KafkaWorkQueueRepository extends WorkQueueRepository {
     public static final String KAFKA_PATH_PREFIX = "/kafka";
-    private Producer<String, JSONObject> kafkaProducer;
+    private static Object kafkaProducerLock = new Object();
+    private static Producer<String, JSONObject> kafkaProducer;
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaWorkQueueRepository.class);
 
     @Override
     public void init(Map config) {
         super.init(config);
 
-        String zkServerNames = config.get(Configuration.ZK_SERVERS) + KAFKA_PATH_PREFIX;
-        LOGGER.info("Kafka Work Queue Repository zkServerNames: " + zkServerNames);
-        Properties props = new Properties();
-        props.put("zk.connect", zkServerNames);
-        props.put("serializer.class", KafkaJsonEncoder.class.getName());
-        ProducerConfig producerConfig = new ProducerConfig(props);
-        try {
-            kafkaProducer = new Producer<String, JSONObject>(producerConfig);
-        } catch (Exception ex) {
-            throw new RuntimeException("Failed to create kafka producer. Have you run /opt/lumify/kafka-clear.sh?", ex);
+        synchronized (kafkaProducerLock) {
+            if (kafkaProducer == null) {
+                String zkServerNames = config.get(Configuration.ZK_SERVERS) + KAFKA_PATH_PREFIX;
+                LOGGER.info("Kafka Work Queue Repository zkServerNames: " + zkServerNames);
+                Properties props = new Properties();
+                props.put("zk.connect", zkServerNames);
+                props.put("serializer.class", KafkaJsonEncoder.class.getName());
+                ProducerConfig producerConfig = new ProducerConfig(props);
+                try {
+                    kafkaProducer = new Producer<String, JSONObject>(producerConfig);
+                } catch (Exception ex) {
+                    throw new RuntimeException("Failed to create kafka producer. Have you run /opt/lumify/kafka-clear.sh?", ex);
+                }
+            }
         }
     }
 
