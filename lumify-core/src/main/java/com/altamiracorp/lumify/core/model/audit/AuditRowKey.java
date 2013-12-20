2b4620e5045b7e7ca74dae424957e70a2ee7cf33
Susan Feng <susan.feng@altamiracorp.com>
2013-12-20 11:05:44 -0500

    made date time zone UTC

--- lumify-core/src/main/java/com/altamiracorp/lumify/core/model/audit/AuditRowKey.java
+++ lumify-core/src/main/java/com/altamiracorp/lumify/core/model/audit/AuditRowKey.java
@@ -5,20 +5,22 @@ import com.altamiracorp.lumify.core.util.RowKeyHelper;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class AuditRowKey extends RowKey {
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
     public AuditRowKey(String rowKey) {
         super(rowKey);
+        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
     }
 
     public static AuditRowKey build(String vertexId) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         Date date = new Date();
         return new AuditRowKey(RowKeyHelper.buildMinor(vertexId, dateFormat.format(date)));
     }
 
     public static AuditRowKey build (String sourceId, String destId) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         Date date = new Date();
         String prefix = sourceId + ":" + destId;
         return new AuditRowKey(RowKeyHelper.buildMinor(prefix, dateFormat.format(date)));
