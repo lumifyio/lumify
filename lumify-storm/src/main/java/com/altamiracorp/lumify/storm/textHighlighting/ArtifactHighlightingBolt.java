5e174eaa53fa0a3d2308b7096f8db1c4d2cf7f35
Susan Feng <susan.feng@altamiracorp.com>
2014-01-15 16:14:59 -0500

    fix artifact highlighting

--- lumify-storm/src/main/java/com/altamiracorp/lumify/storm/textHighlighting/ArtifactHighlightingBolt.java
+++ lumify-storm/src/main/java/com/altamiracorp/lumify/storm/textHighlighting/ArtifactHighlightingBolt.java
@@ -2,13 +2,14 @@ package com.altamiracorp.lumify.storm.textHighlighting;
 
 import backtype.storm.tuple.Tuple;
 import com.altamiracorp.lumify.core.model.artifact.Artifact;
-import com.altamiracorp.lumify.core.model.graph.GraphVertex;
+import com.altamiracorp.lumify.core.model.ontology.OntologyProperty;
 import com.altamiracorp.lumify.core.model.ontology.PropertyName;
 import com.altamiracorp.lumify.core.model.termMention.TermMentionModel;
 import com.altamiracorp.lumify.core.model.termMention.TermMentionRepository;
 import com.altamiracorp.lumify.core.util.LumifyLogger;
 import com.altamiracorp.lumify.core.util.LumifyLoggerFactory;
 import com.altamiracorp.lumify.storm.BaseTextProcessingBolt;
+import com.altamiracorp.securegraph.Vertex;
 import com.google.inject.Inject;
 import org.json.JSONObject;
 
@@ -22,28 +23,28 @@ public class ArtifactHighlightingBolt extends BaseTextProcessingBolt {
         final JSONObject json = getJsonFromTuple(input);
         final String graphVertexId = json.getString("graphVertexId");
 
-        GraphVertex graphVertex = graph.findVertex(graphVertexId, getUser());
+        Vertex graphVertex = graph.getVertex(graphVertexId, getUser().getAuthorizations());
         if (graphVertex != null) {
-            String artifactRowKey = (String) graphVertex.getProperty(PropertyName.ROW_KEY);
+            String artifactRowKey = (String) graphVertex.getPropertyValue(PropertyName.ROW_KEY.toString(), 0);
             LOGGER.debug("Processing graph vertex [%s] for artifact: %s", graphVertex.getId(), artifactRowKey);
 
-            Iterable<TermMentionModel> termMentions = termMentionRepository.findByGraphVertexId(graphVertex.getId(), getUser());
+            Iterable<TermMentionModel> termMentions = termMentionRepository.findByGraphVertexId(graphVertex.getId().toString(), getUser());
             performHighlighting(artifactRowKey, graphVertex, termMentions);
         } else {
             LOGGER.warn("Could not find vertex with id: %s", graphVertexId);
         }
     }
 
-    private void performHighlighting(final String rowKey, final GraphVertex vertex, final Iterable<TermMentionModel> termMentions) throws Exception {
+    private void performHighlighting(final String rowKey, final Vertex vertex, final Iterable<TermMentionModel> termMentions) throws Exception {
         String text = getText(vertex);
         String highlightedText = entityHighlighter.getHighlightedText(text, termMentions, getUser());
+        OntologyProperty highlightedTextHdfsPath = ontologyRepository.getProperty(PropertyName.HIGHLIGHTED_TEXT_HDFS_PATH.toString(), getUser());
 
         Artifact artifact = new Artifact(rowKey);
         artifact.getMetadata().setHighlightedText(highlightedText);
         artifactRepository.save(artifact, getUser().getModelUserContext());
 
-        vertex.removeProperty(PropertyName.HIGHLIGHTED_TEXT_HDFS_PATH.toString());
-        graph.save(vertex, getUser());
+        vertex.removeProperty(highlightedTextHdfsPath.getId().toString(), PropertyName.HIGHLIGHTED_TEXT_HDFS_PATH.toString());
     }
 
     @Inject
