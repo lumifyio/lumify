6ab105202289f0e9c30acb8dbef6ceb09b0f3071
Susan Feng <susan.feng@altamiracorp.com>
2014-01-15 16:14:59 -0500

    fix artifact highlighting

--- lumify-storm/src/main/java/com/altamiracorp/lumify/storm/BaseTextProcessingBolt.java
+++ lumify-storm/src/main/java/com/altamiracorp/lumify/storm/BaseTextProcessingBolt.java
@@ -1,8 +1,8 @@
 package com.altamiracorp.lumify.storm;
 
-import com.altamiracorp.lumify.core.model.graph.GraphVertex;
 import com.altamiracorp.lumify.core.model.ontology.PropertyName;
 import com.altamiracorp.lumify.core.model.artifact.Artifact;
+import com.altamiracorp.securegraph.Vertex;
 import org.apache.commons.io.IOUtils;
 
 import java.io.ByteArrayInputStream;
@@ -11,15 +11,15 @@ import java.io.InputStream;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public abstract class BaseTextProcessingBolt extends BaseLumifyBolt {
-    protected InputStream getInputStream(GraphVertex graphVertex) throws Exception {
+    protected InputStream getInputStream(Vertex graphVertex) throws Exception {
         checkNotNull(graphVertex, "graphVertex cannot be null");
 
         InputStream textIn;
-        String textHdfsPath = (String) graphVertex.getProperty(PropertyName.TEXT_HDFS_PATH);
+        String textHdfsPath = (String) graphVertex.getPropertyValue(PropertyName.TEXT_HDFS_PATH.toString(), 0);
         if (textHdfsPath != null) {
             textIn = openFile(textHdfsPath);
         } else {
-            String artifactRowKey = (String) graphVertex.getProperty(PropertyName.ROW_KEY);
+            String artifactRowKey = (String) graphVertex.getPropertyValue(PropertyName.ROW_KEY.toString(), 0);
             Artifact artifact = artifactRepository.findByRowKey(artifactRowKey, getUser().getModelUserContext());
             String text = artifact.getMetadata().getText();
             if (text == null) {
@@ -30,7 +30,7 @@ public abstract class BaseTextProcessingBolt extends BaseLumifyBolt {
         return textIn;
     }
 
-    protected String getText(GraphVertex graphVertex) throws Exception {
+    protected String getText(Vertex graphVertex) throws Exception {
         return IOUtils.toString(getInputStream(graphVertex));
     }
 }
