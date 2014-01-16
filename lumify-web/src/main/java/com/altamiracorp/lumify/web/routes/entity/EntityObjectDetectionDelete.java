2ee91c70249938a932b371b59ddac44c3eb116cd
Susan Feng <susan.feng@altamiracorp.com>
2014-01-16 10:04:09 -0500

    fix object detection delete

--- lumify-web/src/main/java/com/altamiracorp/lumify/web/routes/entity/EntityObjectDetectionDelete.java
+++ lumify-web/src/main/java/com/altamiracorp/lumify/web/routes/entity/EntityObjectDetectionDelete.java
@@ -5,16 +5,16 @@ import com.altamiracorp.lumify.core.model.audit.AuditRepository;
 import com.altamiracorp.lumify.core.model.ontology.LabelName;
 import com.altamiracorp.lumify.core.model.ontology.PropertyName;
 import com.altamiracorp.lumify.core.user.User;
+import com.altamiracorp.lumify.core.util.GraphUtil;
 import com.altamiracorp.lumify.web.BaseRequestHandler;
 import com.altamiracorp.miniweb.HandlerChain;
-import com.altamiracorp.securegraph.Graph;
+import com.altamiracorp.securegraph.*;
 import com.google.inject.Inject;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 
 public class EntityObjectDetectionDelete extends BaseRequestHandler {
     private final Graph graph;
@@ -33,29 +33,35 @@ public class EntityObjectDetectionDelete extends BaseRequestHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, HandlerChain chain) throws Exception {
+        // TODO set visibility
+        Visibility visibility = new Visibility("");
         JSONObject jsonObject = new JSONObject(getRequiredParameter(request, "objectInfo"));
         User user = getUser(request);
 
         // Delete just the relationship if vertex has more than one relationship otherwise delete vertex
         String graphVertexId = jsonObject.getString("graphVertexId");
         JSONObject obj = new JSONObject();
-        obj.put("entityVertex", graph.findVertex(graphVertexId, user).toJson());
-        Map<GraphRelationship, GraphVertex> relationships = graph.getRelationships(graphVertexId, user);
-        GraphVertex artifactVertex = graph.findVertex(jsonObject.getString("artifactId"), user);
-        if (relationships.size() > 1) {
+        Vertex vertex = graph.getVertex(graphVertexId, user.getAuthorizations());
+        obj.put("entityVertex", GraphUtil.toJson(vertex));
+
+        Vertex artifactVertex = graph.getVertex(jsonObject.getString("artifactId"), user.getAuthorizations());
+        Iterable<Edge> edges = artifactVertex.getEdges(vertex, Direction.BOTH, LabelName.CONTAINS_IMAGE_OF.toString(), user.getAuthorizations());
+        if (edges.iterator().hasNext()) {
             String edgeId = artifactVertex.getId() + ">" + graphVertexId + "|" + LabelName.CONTAINS_IMAGE_OF.toString();
             obj.put("edgeId", edgeId);
-            graph.removeRelationship(artifactVertex.getId(), graphVertexId, LabelName.CONTAINS_IMAGE_OF.toString(), user);
+            while (edges.iterator().hasNext()) {
+                graph.removeEdge(edges.iterator().next(), user.getAuthorizations());
+            }
         } else {
             // TODO: replace "" when we implement commenting on ui
-            auditRepository.auditEntity(AuditAction.DELETE.toString(), graphVertexId, artifactVertex.getId(), jsonObject.getString("title"),
+            auditRepository.auditEntity(AuditAction.DELETE.toString(), graphVertexId, artifactVertex.getId().toString(), jsonObject.getString("title"),
                     jsonObject.getString("_conceptType"), "", "", user);
-            graph.remove(graphVertexId, user);
+            graph.removeVertex(graphVertexId, user.getAuthorizations());
             obj.put("remove", true);
         }
 
         // Delete the appropriate detected object json object from the array and reset the property
-        JSONArray detectedObjects = new JSONArray(artifactVertex.getProperty(PropertyName.DETECTED_OBJECTS).toString());
+        JSONArray detectedObjects = new JSONArray(artifactVertex.getPropertyValue(PropertyName.DETECTED_OBJECTS.toString(), 0).toString());
         boolean deleted = false;
         for (int i = 0; i < detectedObjects.length(); i++) {
             JSONObject detectedObject = detectedObjects.getJSONObject(i);
@@ -68,13 +74,12 @@ public class EntityObjectDetectionDelete extends BaseRequestHandler {
         if (!deleted) {
             throw new RuntimeException("Tag was not found in the list of detected objects");
         }
-        artifactVertex.setProperty(PropertyName.DETECTED_OBJECTS, detectedObjects.toString());
-        graph.save(artifactVertex, user);
+        artifactVertex.setProperty(PropertyName.DETECTED_OBJECTS.toString(), detectedObjects.toString(), visibility);
 
         // TODO: replace "" when we implement commenting on ui
         auditRepository.auditEntityProperties(AuditAction.UPDATE.toString(), artifactVertex, PropertyName.DETECTED_OBJECTS.toString(), "", "", user);
 
-        JSONObject updatedArtifactVertex = entityHelper.formatUpdatedArtifactVertexProperty(artifactVertex.getId(), PropertyName.DETECTED_OBJECTS.toString(), artifactVertex.getProperty(PropertyName.DETECTED_OBJECTS));
+        JSONObject updatedArtifactVertex = entityHelper.formatUpdatedArtifactVertexProperty(artifactVertex.getId().toString(), PropertyName.DETECTED_OBJECTS.toString(), artifactVertex.getPropertyValue(PropertyName.DETECTED_OBJECTS.toString(), 0));
         obj.put("updatedArtifactVertex", updatedArtifactVertex);
 
         //TODO: Overwrite old ElasticSearch index with new info
