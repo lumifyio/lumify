25f61a5769012ad904ce1072b525a630c80bae5b
Susan Feng <susan.feng@altamiracorp.com>
2014-01-16 10:50:53 -0500

    find path

--- lumify-web/src/main/java/com/altamiracorp/lumify/web/routes/graph/GraphFindPath.java
+++ lumify-web/src/main/java/com/altamiracorp/lumify/web/routes/graph/GraphFindPath.java
@@ -1,23 +1,26 @@
 package com.altamiracorp.lumify.web.routes.graph;
 
 import com.altamiracorp.lumify.core.user.User;
-import com.altamiracorp.lumify.core.model.graph.GraphRepository;
-import com.altamiracorp.lumify.core.model.graph.GraphVertex;
+import com.altamiracorp.lumify.core.util.GraphUtil;
 import com.altamiracorp.lumify.web.BaseRequestHandler;
 import com.altamiracorp.miniweb.HandlerChain;
+import com.altamiracorp.securegraph.Graph;
+import com.altamiracorp.securegraph.Vertex;
 import com.google.inject.Inject;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Iterator;
 import java.util.List;
 
 public class GraphFindPath extends BaseRequestHandler {
-    private final GraphRepository graphRepository;
+    private final Graph graph;
 
     @Inject
-    public GraphFindPath(final GraphRepository repo) {
-        graphRepository = repo;
+    public GraphFindPath(final Graph graph) {
+        this.graph = graph;
     }
 
     @Override
@@ -36,22 +39,33 @@ public class GraphFindPath extends BaseRequestHandler {
         final String destGraphVertexId = getRequiredParameter(request, "destGraphVertexId");
         final Integer hops = Integer.parseInt(getRequiredParameter(request, "hops"));
 
-        GraphVertex sourceVertex = graphRepository.findVertex(sourceGraphVertexId, user);
+        Vertex sourceVertex = graph.getVertex(sourceGraphVertexId, user.getAuthorizations());
         if (sourceVertex == null) {
             response.sendError(HttpServletResponse.SC_NOT_FOUND, "Source vertex not found");
             return;
         }
 
-        GraphVertex destVertex = graphRepository.findVertex(destGraphVertexId, user);
+        Vertex destVertex = graph.getVertex(destGraphVertexId, user.getAuthorizations());
         if (destVertex == null) {
             response.sendError(HttpServletResponse.SC_NOT_FOUND, "Destination vertex not found");
             return;
         }
 
-        List<List<GraphVertex>> vertices = graphRepository.findPath(sourceVertex, destVertex, depth, hops, user);
+        Iterator<List<Object>> verticesIds = graph.findPaths(sourceVertex, destVertex, hops, user.getAuthorizations()).iterator();
 
         JSONObject resultsJson = new JSONObject();
-        resultsJson.put("paths", GraphVertex.toJsonPath(vertices));
+        JSONArray pathResults = new JSONArray();
+
+        while (verticesIds.hasNext()) {
+            List<Object> ids = verticesIds.next();
+            JSONArray vertices = new JSONArray();
+            for (Object id : ids) {
+                vertices.put(GraphUtil.toJson(graph.getVertex(id, user.getAuthorizations())));
+            }
+            pathResults.put(vertices);
+        }
+
+        resultsJson.put("paths", pathResults);
 
         respondWithJson(response, resultsJson);
     }
