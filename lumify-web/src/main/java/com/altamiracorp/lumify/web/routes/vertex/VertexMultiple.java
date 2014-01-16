60404767d0adeb7552f511b28c21022618d9eda5
Susan Feng <susan.feng@altamiracorp.com>
2014-01-16 10:44:46 -0500

    vertex multiple

--- lumify-web/src/main/java/com/altamiracorp/lumify/web/routes/vertex/VertexMultiple.java
+++ lumify-web/src/main/java/com/altamiracorp/lumify/web/routes/vertex/VertexMultiple.java
@@ -1,23 +1,24 @@
 package com.altamiracorp.lumify.web.routes.vertex;
 
-import com.altamiracorp.lumify.core.model.graph.GraphRepository;
-import com.altamiracorp.lumify.core.model.graph.GraphVertex;
 import com.altamiracorp.lumify.core.user.User;
+import com.altamiracorp.lumify.core.util.GraphUtil;
 import com.altamiracorp.lumify.web.BaseRequestHandler;
 import com.altamiracorp.miniweb.HandlerChain;
+import com.altamiracorp.securegraph.Graph;
+import com.altamiracorp.securegraph.Vertex;
 import com.google.inject.Inject;
+import org.json.JSONArray;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class VertexMultiple extends BaseRequestHandler {
-    private final GraphRepository graphRepository;
+    private final Graph graph;
 
     @Inject
-    public VertexMultiple(final GraphRepository repo) {
-        graphRepository = repo;
+    public VertexMultiple(final Graph graph) {
+        this.graph = graph;
     }
 
     @Override
@@ -25,16 +26,27 @@ public class VertexMultiple extends BaseRequestHandler {
         String[] vertexIds = request.getParameterValues("vertexIds[]");
         User user = getUser(request);
 
-        List<GraphVertex> graphVertices = graphRepository.findVertices(vertexIds, user);
+        List<Vertex> graphVertices = findAllVertices(vertexIds, user);
 
-        Iterator<GraphVertex> i = graphVertices.iterator();
+        Iterator<Vertex> i = graphVertices.iterator();
+        JSONArray results = new JSONArray();
         while (i.hasNext()) {
-            GraphVertex vertex = i.next();
+            Vertex vertex = i.next();
             if (vertex == null) {
                 i.remove();
+            } else {
+                results.put(GraphUtil.toJson(vertex));
             }
         }
 
-        respondWithJson(response, GraphVertex.toJson(graphVertices));
+        respondWithJson(response,results);
+    }
+
+    private List<Vertex> findAllVertices (String[] vertexIds, User user) {
+        List<Vertex> vertices = new ArrayList<Vertex>();
+        for (String id : vertexIds) {
+            vertices.add(graph.getVertex(id, user.getAuthorizations()));
+        }
+        return vertices;
     }
 }
