f323c347f37d49b2c045da7b7e9e5d1eb864051c
Joe Ferner <joe@fernsroth.com>
2014-01-16 10:32:52 -0500

    find relationships

--- lumify-web/src/main/java/com/altamiracorp/lumify/web/routes/entity/EntityRelationships.java
+++ lumify-web/src/main/java/com/altamiracorp/lumify/web/routes/entity/EntityRelationships.java
@@ -3,17 +3,14 @@ package com.altamiracorp.lumify.web.routes.entity;
 import com.altamiracorp.lumify.core.user.User;
 import com.altamiracorp.lumify.web.BaseRequestHandler;
 import com.altamiracorp.miniweb.HandlerChain;
-import com.altamiracorp.securegraph.Direction;
-import com.altamiracorp.securegraph.Edge;
-import com.altamiracorp.securegraph.Graph;
+import com.altamiracorp.securegraph.*;
 import com.google.inject.Inject;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class EntityRelationships extends BaseRequestHandler {
     private final Graph graph;
@@ -40,8 +37,7 @@ public class EntityRelationships extends BaseRequestHandler {
 
         JSONArray resultsJson = new JSONArray();
 
-        Iterable<Edge> edges = graph.getRelationships(allIds, user);
-
+        Collection<Edge> edges = getAllEdges(allIds, user.getAuthorizations());
         for (Edge edge : edges) {
             JSONObject rel = new JSONObject();
             rel.put("from", edge.getVertexId(Direction.OUT));
@@ -51,5 +47,28 @@ public class EntityRelationships extends BaseRequestHandler {
         }
 
         respondWithJson(response, resultsJson);
+    }
+
+    private Collection<Edge> getAllEdges(List<String> allVertexIds, Authorizations authorizations) {
+        Set<Edge> results = new HashSet<Edge>();
+        Map<String, Vertex> vertices = toVertices(allVertexIds, authorizations);
+
+        for (Vertex sourceVertex : vertices.values()) {
+            for (Vertex destVertex : vertices.values()) {
+                Iterable<Edge> edges = sourceVertex.getEdges(destVertex, Direction.BOTH, authorizations);
+                for (Edge edge : edges) {
+                    results.add(edge);
+                }
+            }
+        }
+        return results;
+    }
+
+    private Map<String, Vertex> toVertices(List<String> allVertexIds, Authorizations authorizations) {
+        Map<String, Vertex> vertices = new HashMap<String, Vertex>();
+        for (String vertexId : allVertexIds) {
+            vertices.put(vertexId, graph.getVertex(vertexId, authorizations));
+        }
+        return vertices;
     }
 }
