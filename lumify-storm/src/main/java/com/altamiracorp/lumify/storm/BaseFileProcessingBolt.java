f8a3bc395d577afba44834e09938e3af0ef592a0
Susan Feng <susan.feng@altamiracorp.com>
2014-01-21 10:59:01 -0500

    [#64233382] add nullable for ContentTypeExtractor

--- lumify-storm/src/main/java/com/altamiracorp/lumify/storm/BaseFileProcessingBolt.java
+++ lumify-storm/src/main/java/com/altamiracorp/lumify/storm/BaseFileProcessingBolt.java
@@ -164,7 +164,7 @@ public abstract class BaseFileProcessingBolt extends BaseLumifyBolt {
     }
 
     @Inject(optional = true)
-    public void setContentTypeExtractor(ContentTypeExtractor contentTypeExtractor) {
+    public void setContentTypeExtractor(@Nullable ContentTypeExtractor contentTypeExtractor) {
         this.contentTypeExtractor = contentTypeExtractor;
     }
 }
