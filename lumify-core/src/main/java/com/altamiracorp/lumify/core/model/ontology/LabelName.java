4b90b9296c05daa7ec84bf21a289beaad71f029b
Rebecca Matous <rmatous@nearinfinity.com>
2014-01-22 13:24:16 -0500

    Abstracted Facebook and twitter specific ontology stuff from enterprise

--- lumify-core/src/main/java/com/altamiracorp/lumify/core/model/ontology/LabelName.java
+++ lumify-core/src/main/java/com/altamiracorp/lumify/core/model/ontology/LabelName.java
@@ -4,10 +4,6 @@ public enum LabelName {
     HAS_PROPERTY("hasProperty"),
     HAS_EDGE("hasEdge"),
     IS_A("isA"),
-    ENTITY_HAS_IMAGE_HANDLE_PHOTO("entityHasImageTweeterProfileImage"),
-    HANDLE_CONTAINS_IMAGE_OF("tweeterProfileImageContainsImageOfEntity"),
-    ENTITY_HAS_IMAGE_PROFILE_PHOTO("entityHasImageFacebookProfileImage"),
-    PROFILE_CONTAINS_IMAGE_OF_ENTITY("facebookProfileImageContainsImageOfEntity"),
     ENTITY_HAS_IMAGE_RAW("entityHasImageRaw"),
     RAW_HAS_ENTITY("rawHasEntity"),
     RAW_CONTAINS_IMAGE_OF_ENTITY("rawContainsImageOfEntity");
