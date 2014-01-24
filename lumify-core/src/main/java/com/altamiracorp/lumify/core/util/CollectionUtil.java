e730a75dfea8055621b2dbe73ffc106f115dd4c3
Gordon Shankman <gordon.shankman@altamiracorp.com>
2014-01-24 15:28:53 -0500

    Added trySingle() methods to CollectionUtil that return a default value if there are 0 or > 1 elements in the provided Iterable; modified TermExtractionBolt to use this method instead of single().  Updated DocumentMappingEntityExtractorWorker to handle StreamingPropertyValue objects.

--- lumify-core/src/main/java/com/altamiracorp/lumify/core/util/CollectionUtil.java
+++ lumify-core/src/main/java/com/altamiracorp/lumify/core/util/CollectionUtil.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 
 public class CollectionUtil {
-    public static <T> List<T> toList(Iterable<T> it) {
+    public static <T> List<T> toList(final Iterable<T> it) {
         List<T> list = new ArrayList<T>();
         for (T t : it) {
             list.add(t);
@@ -13,18 +13,34 @@ public class CollectionUtil {
         return list;
     }
 
-    public static <T> T single(Iterable<T> it) {
+    public static <T> T single(final Iterable<T> it) {
         Iterator<T> i = it.iterator();
         if (!i.hasNext()) {
-            throw new RuntimeException("Iterable has no items");
+            throw new IllegalStateException("No items found.");
         }
 
         T result = i.next();
 
         if (i.hasNext()) {
-            throw new RuntimeException("Iterable has more than one item");
+            throw new IllegalStateException("More than 1 item found.");
         }
 
         return result;
+    }
+
+    public static <T> T trySingle(final Iterable<T> it) {
+        return trySingle(it, null);
+    }
+
+    public static <T> T trySingle(final Iterable<T> it, final T defaultValue) {
+        try {
+            return single(it);
+        } catch (IllegalStateException ise) {
+            return defaultValue;
+        }
+    }
+
+    private CollectionUtil() {
+        throw new UnsupportedOperationException("Don't construct a utility class.");
     }
 }
