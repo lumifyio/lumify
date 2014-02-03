682dc73c34ed33ec4e9041186eaa927ea49fea26
Joe Ferner <joe@fernsroth.com>
2014-02-03 12:04:29 -0500

    visibility on set property edge

--- lumify-web/src/main/java/com/altamiracorp/lumify/web/routes/relationship/SetRelationshipProperty.java
+++ lumify-web/src/main/java/com/altamiracorp/lumify/web/routes/relationship/SetRelationshipProperty.java
@@ -4,22 +4,21 @@ import com.altamiracorp.lumify.core.model.audit.AuditAction;
 import com.altamiracorp.lumify.core.model.audit.AuditRepository;
 import com.altamiracorp.lumify.core.model.ontology.OntologyProperty;
 import com.altamiracorp.lumify.core.model.ontology.OntologyRepository;
-import com.altamiracorp.lumify.core.model.ontology.PropertyName;
+import com.altamiracorp.lumify.core.security.VisibilityTranslator;
 import com.altamiracorp.lumify.core.user.User;
 import com.altamiracorp.lumify.core.util.GraphUtil;
 import com.altamiracorp.lumify.core.util.LumifyLogger;
 import com.altamiracorp.lumify.core.util.LumifyLoggerFactory;
 import com.altamiracorp.lumify.web.BaseRequestHandler;
-import com.altamiracorp.lumify.web.routes.vertex.VertexProperties;
 import com.altamiracorp.miniweb.HandlerChain;
-import com.altamiracorp.securegraph.*;
+import com.altamiracorp.securegraph.Edge;
+import com.altamiracorp.securegraph.ElementMutation;
+import com.altamiracorp.securegraph.Graph;
 import com.google.inject.Inject;
 import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SetRelationshipProperty extends BaseRequestHandler {
     private static final LumifyLogger LOGGER = LumifyLoggerFactory.getLogger(SetRelationshipProperty.class);
@@ -27,12 +26,18 @@ public class SetRelationshipProperty extends BaseRequestHandler {
     private final Graph graph;
     private final OntologyRepository ontologyRepository;
     private final AuditRepository auditRepository;
+    private VisibilityTranslator visibilityTranslator;
 
     @Inject
-    public SetRelationshipProperty(final OntologyRepository ontologyRepository, final Graph graph, final AuditRepository auditRepository) {
+    public SetRelationshipProperty(
+            final OntologyRepository ontologyRepository,
+            final Graph graph,
+            final AuditRepository auditRepository,
+            final VisibilityTranslator visibilityTranslator) {
         this.ontologyRepository = ontologyRepository;
         this.graph = graph;
         this.auditRepository = auditRepository;
+        this.visibilityTranslator = visibilityTranslator;
     }
 
     @Override
@@ -42,6 +47,7 @@ public class SetRelationshipProperty extends BaseRequestHandler {
         final String sourceId = getRequiredParameter(request, "source");
         final String destId = getRequiredParameter(request, "dest");
         final String edgeId = getRequiredParameter(request, "edgeId");
+        final String visibilitySource = getRequiredParameter(request, "visibilitySource");
 
         User user = getUser(request);
 
@@ -59,19 +65,14 @@ public class SetRelationshipProperty extends BaseRequestHandler {
             return;
         }
         Edge edge = graph.getEdge(edgeId, user.getAuthorizations());
-        // TODO set visibility
         Object oldValue = edge.getPropertyValue(propertyName, 0);
-        edge.setProperty(propertyName, value, new Visibility(""));
+        ElementMutation<Edge> graphEdgeMutation = GraphUtil.setProperty(edge, propertyName, value, visibilitySource, this.visibilityTranslator);
+        graphEdgeMutation.save();
+
         // TODO: replace "" when we implement commenting on ui
         auditRepository.auditRelationshipProperties(AuditAction.DELETE.toString(), sourceId, destId, propertyName, oldValue, edge, "", "", user);
 
-        // TODO get all properties from all edges?
-        List<com.altamiracorp.securegraph.Property> properties = new ArrayList<com.altamiracorp.securegraph.Property>();
-        for (com.altamiracorp.securegraph.Property p : edge.getProperties()) {
-            properties.add(p);
-        }
-        JSONObject resultsJson = GraphUtil.toJson(properties);
-
+        JSONObject resultsJson = GraphUtil.toJson(edge.getProperties());
         respondWithJson(response, resultsJson);
     }
 }
