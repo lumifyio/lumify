3b2206116b56d2ce3455b9b3f85c6461aebd4b57
Jason Harwig <jason@pinepointsoftware.com>
2014-02-20 12:12:40 -0600

    Add functional test for dragging onto graph

--- lumify-web-war/src/main/webapp/test/functional/spec/searchSpec.js
+++ lumify-web-war/src/main/webapp/test/functional/spec/searchSpec.js
@@ -1,38 +1,31 @@
-// TODO: refactor out
-var USER = 'selenium',
-    PASS = 'password',
-    menubarAnimationFinished = "$('.menubar-pane').offset().left >= -1",
-    openSearchAnimationFinished = "$('.search-pane').offset().left >= ($('.menubar-pane').width() - 5)",
-    closeSearchAnimationFinished = "$('.search-pane').offset().left < (-1 * $('.search-pane').width())";
+var utils = require('../utils');
 
 describe.only('Search', function () {
 
-    before(function() {
-        var self = this,
-            browser = this.browser;
-
-        // TODO: refactor login out
-        return this.browser
-          .get('https://localhost:8443')
-          .sessionCapabilities().then(function(c) { self.capabilities = c; })
-          .waitForElementByCss('.login button')
-          .text().should.become('Log In')
-          .elementByCss('.username').type(USER)
-          .elementByCss('.password').type(PASS)
-          .elementByCss('.login button').click()
-          .waitForElementByCss('.menubar-pane')
-          .waitFor(this.asserters.jsCondition(menubarAnimationFinished) , 2000)
-    })
+    before(utils.login);
 
     it('Should be able to toggle search with menubar', function () {
         return this.browser
           .waitForElementByCss('.menubar-pane .search a').should.eventually.be.ok
           .click()
-          .waitFor(this.asserters.jsCondition(openSearchAnimationFinished) , 2000).should.eventually.be.ok
+          .waitFor(this.asserters.jsCondition(utils.animations.openSearchAnimationFinished) , 2000).should.eventually.be.ok
           .waitForElementByCss('.search-query:focus') 
           .elementByCss('.menubar-pane .search a')
           .click()
-          .waitFor(this.asserters.jsCondition(closeSearchAnimationFinished) , 2000)
+          .waitFor(this.asserters.jsCondition(utils.animations.closeSearchAnimationFinished) , 2000)
+    })
+
+    it('Should be able to toggle search with shortcut', function () {
+        return this.browser
+          .waitForElementByCss('.menubar-pane .search a').should.eventually.be.ok
+          .elementByTagName('body')
+          .sendKeys('/')
+          .waitFor(this.asserters.jsCondition(utils.animations.openSearchAnimationFinished) , 2000).should.eventually.be.ok
+          .waitForElementByCss('.search-query:focus') 
+          .elementByTagName('body')
+          .sendKeys(this.KEYS.Escape)
+          .sendKeys(this.KEYS.Escape)
+          .waitFor(this.asserters.jsCondition(utils.animations.closeSearchAnimationFinished) , 2000)
     })
 
     it('Should be able to search *', function() {
@@ -41,10 +34,10 @@ describe.only('Search', function () {
         return browser
           .waitForElementByCss('.menubar-pane .search a').should.eventually.be.ok
           .click()
-          .waitFor(this.asserters.jsCondition(openSearchAnimationFinished) , 2000).should.eventually.exist
+          .waitFor(this.asserters.jsCondition(utils.animations.openSearchAnimationFinished) , 2000).should.eventually.exist
           .waitForElementByCss('.search-query:focus').should.eventually.exist
           .type('*')
-          .sendKeys(this.KEYS.Return)
+          .keys(this.KEYS.Return)
           .elementByCss('.search-results').getComputedCss('display').should.become('none')
           .then(function() {
               var concept = 'Raw',
@@ -59,6 +52,35 @@ describe.only('Search', function () {
           })
           .elementByCss('.search-results').getComputedCss('display').should.become('block')
           .waitFor(this.asserters.jsCondition("$('.search-results .vertex-item').length === 8") , 500).should.eventually.be.ok
+    })
+
+    it('Should be able to drag result to graph', function() {
+        return this.browser
+            .elementByCss('.vertex-item')
+            .moveTo()
+            .buttonDown()
+            .elementByCss('.graph-pane')
+            .moveTo(550, 100)
+            .sleep(100)
+            .moveTo(600, 150)
+            .sleep(100)
+            .buttonUp()
+            .waitFor(this.asserters.jsCondition("$('.cytoscape-container').cytoscape('get').nodes().length === 1"), 1000)
+            .should.eventually.be.ok
+            .elementByCss('.graph-pane .controls button[data-event=fit]')
+            .click()
+            .elementByCss('.graph-pane')
+            .moveTo(625,275)
+            .buttonDown()
+            .buttonUp()
+            .sleep(1000)
+            .elementByTagName('body')
+            .sendKeys(this.KEYS.Delete)
+            .sendKeys(this.KEYS['Back space'])
+            .waitFor(this.asserters.jsCondition("$('.cytoscape-container').cytoscape('get').nodes().length === 0"), 1000)
+            .should.eventually.be.ok
+            .waitForElementByCss('.menubar-pane .activity.animating', 1000).should.eventually.exist
+            .waitForElementByCss('.menubar-pane .activity:not(.animating)', 10000).should.eventually.exist
     })
 
 })
