2c2325f586312c80e946a8317fd6bd068009494e
Joe Ferner <joe@fernsroth.com>
2014-02-28 15:19:04 -0500

    workspace ids

--- lumify-core/src/main/java/com/altamiracorp/lumify/core/model/user/AuthorizationRepository.java
+++ lumify-core/src/main/java/com/altamiracorp/lumify/core/model/user/AuthorizationRepository.java
@@ -1,11 +1,14 @@
 package com.altamiracorp.lumify.core.model.user;
 
+import java.util.List;
 import java.util.Set;
 
 public interface AuthorizationRepository {
     void addAuthorizationToGraph(final String auth);
 
     void removeAuthorizationFromGraph(final String auth);
+
+    List<String> getGraphAuthorizations();
 
     com.altamiracorp.securegraph.Authorizations createAuthorizations(Set<String> authorizationsSet);
 }
