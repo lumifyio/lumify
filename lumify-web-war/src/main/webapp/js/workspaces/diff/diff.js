f2d5498ab7243d2a4e17c7498317a351d84e0dda
Jason Harwig <jason@pinepointsoftware.com>
2014-03-05 16:48:24 -0600

    Testing publish buttons

--- lumify-web-war/src/main/webapp/js/workspaces/diff/diff.js
+++ lumify-web-war/src/main/webapp/js/workspaces/diff/diff.js
@@ -9,15 +9,37 @@ define([
 
     function Diff() {
 
+        this.defaultAttrs({
+            publishSelector: 'button'
+        })
+
         this.after('initialize', function() {
             this.$node.html(template({diffs:this.attr.diffs}));
 
+            this.on('click', {
+                publishSelector: this.onPublish
+            })
             this.on('diffsChanged', function(event, data) {
                 this.$node.html(template({
                     diffs: data.diffs
                 }));
             })
         });
+
+        this.onPublish = function() {
+            var $target = $(event.target),
+                cls = 'btn-success';
+
+            if ($target.closest('thead').length) {
+                var allButtons = $target.closest('table').find('td button');
+                if ($target.hasClass(cls)) {
+                    allButtons.removeClass(cls);
+                } else {
+                    allButtons.addClass(cls);
+                }
+            }
+            $target.toggleClass(cls)
+        };
     }
 });
 
