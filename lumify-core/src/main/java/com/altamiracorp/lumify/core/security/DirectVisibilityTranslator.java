bba5127f2afd47457e014d9a31181f6a0be2de1a
Joe Ferner <joe@fernsroth.com>
2014-03-05 13:09:00 -0500

    add not

--- lumify-core/src/main/java/com/altamiracorp/lumify/core/security/DirectVisibilityTranslator.java
+++ lumify-core/src/main/java/com/altamiracorp/lumify/core/security/DirectVisibilityTranslator.java
@@ -17,31 +17,50 @@ public class DirectVisibilityTranslator implements VisibilityTranslator {
     public LumifyVisibility toVisibility(JSONObject visibilityJson) {
         StringBuilder visibilityString = new StringBuilder();
 
-        List<String> all = new ArrayList<String>();
+        List<String> required = new ArrayList<String>();
+        List<String> notRequired = new ArrayList<String>();
 
         String source = visibilityJson.optString(JSON_SOURCE);
         if (source != null && source.trim().length() > 0) {
-            all.add(source.trim());
+            required.add(source.trim());
         }
 
         JSONArray workspaces = visibilityJson.optJSONArray(JSON_WORKSPACES);
         if (workspaces != null) {
             for (int i = 0; i < workspaces.length(); i++) {
                 String workspace = workspaces.getString(i);
-                all.add(workspace);
+                required.add(workspace);
             }
         }
 
-        for (int i = 0; i < all.size(); i++) {
-            String additionalRequiredVisibility = all.get(i);
-            if (i > 0) {
+        JSONArray notWorkspaces = visibilityJson.optJSONArray(JSON_NOT_WORKSPACES);
+        if (notWorkspaces != null) {
+            for (int i = 0; i < notWorkspaces.length(); i++) {
+                String workspace = notWorkspaces.getString(i);
+                notRequired.add(workspace);
+            }
+        }
+
+        for (String v : required) {
+            if (visibilityString.length() > 0) {
                 visibilityString.append("&");
             }
             visibilityString
                     .append("(")
-                    .append(additionalRequiredVisibility)
+                    .append(v)
                     .append(")");
         }
+
+        // TODO add when Accumulo supports '!'/'not'
+//        for (String v : notRequired) {
+//            if (visibilityString.length() > 0) {
+//                visibilityString.append("&");
+//            }
+//            visibilityString
+//                    .append("(!")
+//                    .append(v)
+//                    .append(")");
+//        }
 
         return new LumifyVisibility(visibilityString.toString());
     }
