910729fdc70fb1fab0b4293762319a42b725f306
Susan Feng <susan.feng@altamiracorp.com>
2014-03-12 23:37:52 -0400

    only allows edges with PUBLIC flag to be deleted

--- lumify-web/src/main/java/com/altamiracorp/lumify/web/routes/vertex/VertexRelationshipRemoval.java
+++ lumify-web/src/main/java/com/altamiracorp/lumify/web/routes/vertex/VertexRelationshipRemoval.java
@@ -5,11 +5,16 @@ import com.altamiracorp.lumify.core.model.audit.AuditAction;
 import com.altamiracorp.lumify.core.model.audit.AuditRepository;
 import com.altamiracorp.lumify.core.model.ontology.OntologyRepository;
 import com.altamiracorp.lumify.core.model.user.UserRepository;
+import com.altamiracorp.lumify.core.model.workspace.diff.SandboxStatus;
 import com.altamiracorp.lumify.core.security.LumifyVisibility;
 import com.altamiracorp.lumify.core.user.User;
+import com.altamiracorp.lumify.core.util.GraphUtil;
+import com.altamiracorp.lumify.core.util.LumifyLogger;
+import com.altamiracorp.lumify.core.util.LumifyLoggerFactory;
 import com.altamiracorp.lumify.web.BaseRequestHandler;
 import com.altamiracorp.miniweb.HandlerChain;
 import com.altamiracorp.securegraph.Authorizations;
+import com.altamiracorp.securegraph.Edge;
 import com.altamiracorp.securegraph.Graph;
 import com.altamiracorp.securegraph.Vertex;
 import com.google.inject.Inject;
@@ -17,8 +22,10 @@ import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.logging.Logger;
 
 public class VertexRelationshipRemoval extends BaseRequestHandler {
+    private static final LumifyLogger LOGGER = LumifyLoggerFactory.getLogger(VertexRelationshipRemoval.class);
     private final Graph graph;
     private final AuditRepository auditRepository;
     private final OntologyRepository ontologyRepository;
@@ -42,12 +49,23 @@ public class VertexRelationshipRemoval extends BaseRequestHandler {
         final String targetId = getRequiredParameter(request, "targetId");
         final String label = getRequiredParameter(request, "label");
         final String edgeId = getRequiredParameter(request, "edgeId");
+        String workspaceId = getWorkspaceId(request);
 
         User user = getUser(request);
         Authorizations authorizations = getAuthorizations(request, user);
 
         Vertex sourceVertex = graph.getVertex(sourceId, authorizations);
         Vertex destVertex = graph.getVertex(targetId, authorizations);
+        Edge edge = graph.getEdge(edgeId, authorizations);
+
+        SandboxStatus sandboxStatuses = GraphUtil.getSandboxStatus(edge, workspaceId);
+
+        if (sandboxStatuses == SandboxStatus.PUBLIC) {
+            LOGGER.warn("Could not find non-public edge: %s", edgeId);
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            chain.next(request, response);
+            return;
+        }
 
         graph.removeEdge(edgeId, authorizations);
 
