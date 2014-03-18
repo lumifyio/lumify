4c84d22582430a41063e4e7caf5c3ce118a5414d
Jason Harwig <jason@pinepointsoftware.com>
2014-03-18 10:39:08 -0500

    use jshint config in plato report

--- lumify-web-war/src/main/webapp/Gruntfile.js
+++ lumify-web-war/src/main/webapp/Gruntfile.js
@@ -99,13 +99,14 @@ module.exports = function(grunt) {
 
     jshint: {
         options: {
-            browser: true,
-            '-W033': true, // Semicolons
-            '-W040': true, // Ignore Strict violations from flight idioms
+            jshintrc: true
         },
         development: {
             files: {
                 src: ['js/**/*.js']
+            },
+            options: {
+                reporter: require('jshint-stylish')
             }
         },
         ci: {
@@ -123,7 +124,9 @@ module.exports = function(grunt) {
         options: {
             config: ".jscs.json"
         },
-        all: { src: "js/**/*.js" },
+        all: { 
+            src: ["js/**/*.js", "!js/**/three-plugins/*.js"],
+        },
         passing: { 
             src: [
                 "js/lumify.js",
@@ -144,6 +147,13 @@ module.exports = function(grunt) {
         ci: {
             files: {
                 'build/plato': ['js/**/*.js'],
+            },
+            options: {
+                jshint: {
+                    "browser": true,
+                    "-W033": true,
+                    "-W040": true
+                }
             }
         },
     },
