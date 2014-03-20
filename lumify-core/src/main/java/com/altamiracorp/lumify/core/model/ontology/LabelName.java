289ba0c848b3033a9ce515356a911a53c1b7b284
Joe Ferner <joe@fernsroth.com>
2014-03-20 11:28:27 -0400

    fix resolve entity

--- lumify-core/src/main/java/com/altamiracorp/lumify/core/model/ontology/LabelName.java
+++ lumify-core/src/main/java/com/altamiracorp/lumify/core/model/ontology/LabelName.java
@@ -1,12 +1,12 @@
 package com.altamiracorp.lumify.core.model.ontology;
 
 public enum LabelName {
-    HAS_PROPERTY("hasProperty"),
-    HAS_EDGE("hasEdge"),
-    IS_A("isA"),
-    ENTITY_HAS_IMAGE_RAW("entityHasImageRaw"),
-    RAW_HAS_ENTITY("rawHasEntity"),
-    RAW_CONTAINS_IMAGE_OF_ENTITY("rawContainsImageOfEntity");
+    HAS_PROPERTY("http://lumify.io/ontology#hasProperty"),
+    HAS_EDGE("http://lumify.io/ontology#hasEdge"),
+    IS_A("http://lumify.io/ontology#isA"),
+    ENTITY_HAS_IMAGE_RAW("http://lumify.io/dev#entityHasImageRaw"),
+    RAW_HAS_ENTITY("http://lumify.io/dev#rawHasEntity"),
+    RAW_CONTAINS_IMAGE_OF_ENTITY("http://lumify.io/dev#rawContainsImageOfEntity");
 
     private final String text;
 
