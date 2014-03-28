0773de8f804bdab9a93125398444bf86b5257032
Joe Ferner <joe@fernsroth.com>
2014-03-28 10:06:14 -0400

    fix BaseOntologyTest

--- lumify-core/src/test/java/com/altamiracorp/lumify/core/ontology/BaseOntologyTest.java
+++ lumify-core/src/test/java/com/altamiracorp/lumify/core/ontology/BaseOntologyTest.java
@@ -1,8 +1,8 @@
 package com.altamiracorp.lumify.core.ontology;
 
 import com.altamiracorp.lumify.core.model.ontology.Concept;
+import com.altamiracorp.lumify.core.model.ontology.OntologyLumifyProperties;
 import com.altamiracorp.lumify.core.model.ontology.OntologyRepository;
-import com.altamiracorp.lumify.core.model.ontology.PropertyType;
 import com.altamiracorp.lumify.core.user.User;
 import com.altamiracorp.securegraph.Graph;
 import com.altamiracorp.securegraph.Vertex;
@@ -15,8 +15,6 @@ import org.mockito.runners.MockitoJUnitRunner;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -48,8 +46,8 @@ public class BaseOntologyTest {
 
     @Test
     public void testDefineOntology() {
-        when(ontologyRepository.getOrCreateConcept((Concept) isNull(), eq("rootConcept"), eq("rootConcept"))).thenReturn(rootConcept);
-        when(ontologyRepository.getOrCreateConcept(eq(rootConcept), anyString(), eq("Entity"))).thenReturn(entityConcept);
+        when(ontologyRepository.getOrCreateConcept((Concept) isNull(), eq(OntologyRepository.ROOT_CONCEPT_IRI), eq("root"))).thenReturn(rootConcept);
+        when(ontologyRepository.getOrCreateConcept(eq(rootConcept), eq(OntologyRepository.ENTITY_CONCEPT_IRI), eq("thing"))).thenReturn(entityConcept);
 
         baseOntology.defineOntology(user);
     }
@@ -76,15 +74,15 @@ public class BaseOntologyTest {
 
     @Test
     public void testIsOntologyDefinedExceptionWithFalse() {
-        when(ontologyRepository.getConceptById(OntologyRepository.ROOT_CONCEPT_IRI)).thenThrow(new RuntimeException("ontologyTitle", new Throwable("testing exception")));
+        when(ontologyRepository.getConceptById(OntologyRepository.ROOT_CONCEPT_IRI)).thenThrow(new RuntimeException(OntologyLumifyProperties.ONTOLOGY_TITLE.getKey(), new Throwable("testing exception")));
         assertFalse(baseOntology.isOntologyDefined(user));
     }
 
     @Test
     public void testInitializeWhenUndefined() {
         when(ontologyRepository.getConceptById(OntologyRepository.ROOT_CONCEPT_IRI)).thenReturn(null);
-        when(ontologyRepository.getOrCreateConcept((Concept) isNull(), eq("rootConcept"), eq("rootConcept"))).thenReturn(rootConcept);
-        when(ontologyRepository.getOrCreateConcept(eq(rootConcept), anyString(), eq("Entity"))).thenReturn(entityConcept);
+        when(ontologyRepository.getOrCreateConcept((Concept) isNull(), eq(OntologyRepository.ROOT_CONCEPT_IRI), eq("root"))).thenReturn(rootConcept);
+        when(ontologyRepository.getOrCreateConcept(eq(rootConcept), eq(OntologyRepository.ENTITY_CONCEPT_IRI), eq("thing"))).thenReturn(entityConcept);
 
         baseOntology.initialize(user);
     }
