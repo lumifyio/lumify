b82388ab5ffd053e5bfe97b7ea6432e733d7daaa
Susan Feng <susan.feng@altamiracorp.com>
2014-04-11 14:55:05 -0400

    got rid of init mathod

--- lumify-core/src/main/java/com/altamiracorp/lumify/core/model/user/AuthorizationRepository.java
+++ lumify-core/src/main/java/com/altamiracorp/lumify/core/model/user/AuthorizationRepository.java
@@ -6,8 +6,6 @@ import java.util.Set;
 
 public interface AuthorizationRepository {
 
-    void init(Map config);
-
     void addAuthorizationToGraph(final String auth);
 
     void removeAuthorizationFromGraph(final String auth);
