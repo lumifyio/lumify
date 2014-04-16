957e3d812dafc2084c0d56e5579e986160f6fff7
Jason Harwig <jason@pinepointsoftware.com>
2014-04-16 11:51:21 -0500

    mixin

---
+++ lumify-web-war/src/main/webapp/js/util/withFileDrop.js
@@ -0,0 +1,34 @@
+define([], function() {
+    'use strict';
+
+    return withFileDrop;
+
+    function withFileDrop() {
+
+        this.after('initialize', function() {
+            var self = this;
+
+            if (!this.handleFilesDropped) {
+                return console.warn('Implement handleFilesDropped');
+            }
+
+            this.node.ondragover = function() {
+                $(this).addClass('file-hover'); return false; 
+            };
+            this.node.ondragenter = function() {
+                $(this).addClass('file-hover'); return false; 
+            };
+            this.node.ondragleave = function() {
+                $(this).removeClass('file-hover'); return false; 
+            };
+            this.node.ondrop = function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+
+                if (self.$node.hasClass('uploading')) return;
+
+                self.handleFilesDropped(e.dataTransfer.files);
+            };
+        });
+    }
+});
