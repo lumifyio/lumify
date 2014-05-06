1c035e92220ebd90334e9d6f18301aa16dbac985
Joe Ferner <joe@fernsroth.com>
2014-05-06 10:42:55 -0400

    add parameter 'fallbackToPublic' to allow getting public vertices

--- lumify-web/src/main/java/io/lumify/web/routes/vertex/VertexMultiple.java
+++ lumify-web/src/main/java/io/lumify/web/routes/vertex/VertexMultiple.java
@@ -1,19 +1,21 @@
 package io.lumify.web.routes.vertex;
 
+import com.altamiracorp.miniweb.HandlerChain;
+import com.google.inject.Inject;
 import io.lumify.core.config.Configuration;
+import io.lumify.core.exception.LumifyAccessDeniedException;
+import io.lumify.core.exception.LumifyException;
 import io.lumify.core.model.user.UserRepository;
 import io.lumify.core.model.workspace.WorkspaceRepository;
 import io.lumify.core.user.User;
 import io.lumify.core.util.JsonSerializer;
 import io.lumify.web.BaseRequestHandler;
-import com.altamiracorp.miniweb.HandlerChain;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.securegraph.Authorizations;
 import org.securegraph.Graph;
 import org.securegraph.Vertex;
 import org.securegraph.util.ConvertingIterable;
-import com.google.inject.Inject;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -35,10 +37,11 @@ public class VertexMultiple extends BaseRequestHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, HandlerChain chain) throws Exception {
-        String[] vertexStringIds = request.getParameterValues("vertexIds[]");
+        String[] vertexStringIds = getRequiredParameterArray(request, "vertexIds[]");
+        boolean fallbackToPublic = getOptionalParameterBoolean(request, "fallbackToPublic", false);
         User user = getUser(request);
-        Authorizations authorizations = getAuthorizations(request, user);
-        String workspaceId = getActiveWorkspaceId(request);
+        Authorizations authorizations = getAuthorizations(request, fallbackToPublic, user);
+        String workspaceId = getWorkspaceId(request);
 
         Iterable<Object> vertexIds = new ConvertingIterable<String, Object>(toIterable(vertexStringIds)) {
             @Override
@@ -56,5 +59,29 @@ public class VertexMultiple extends BaseRequestHandler {
         }
 
         respondWithJson(response, results);
+    }
+
+    private Authorizations getAuthorizations(HttpServletRequest request, boolean fallbackToPublic, User user) {
+        Authorizations authorizations;
+        try {
+            authorizations = getAuthorizations(request, user);
+        } catch (LumifyAccessDeniedException ex) {
+            if (fallbackToPublic) {
+                authorizations = getUserRepository().getAuthorizations(user);
+            } else {
+                throw ex;
+            }
+        }
+        return authorizations;
+    }
+
+    private String getWorkspaceId(HttpServletRequest request) {
+        String workspaceId;
+        try {
+            workspaceId = getActiveWorkspaceId(request);
+        } catch (LumifyException ex) {
+            workspaceId = null;
+        }
+        return workspaceId;
     }
 }
