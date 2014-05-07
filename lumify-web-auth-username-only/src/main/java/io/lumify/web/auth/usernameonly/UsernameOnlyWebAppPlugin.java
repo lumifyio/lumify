d6cebc7b7fb7c25d5c35a9241a1c66cd8646b8e3
Jason Harwig <jason@pinepointsoftware.com>
2014-05-07 09:56:17 -0500

    Remove unnecessary routes in usernameonly plugin

--- lumify-web-auth-username-only/src/main/java/io/lumify/web/auth/usernameonly/UsernameOnlyWebAppPlugin.java
+++ lumify-web-auth-username-only/src/main/java/io/lumify/web/auth/usernameonly/UsernameOnlyWebAppPlugin.java
@@ -30,10 +30,12 @@ public class UsernameOnlyWebAppPlugin implements WebAppPlugin {
     public void init(WebApp app, ServletConfig config, Handler authenticationHandler) {
         StaticResourceHandler jsHandler = new StaticResourceHandler(this.getClass(), "/username-only/authentication.js", "application/javascript");
         StaticResourceHandler loginTemplateHandler = new StaticResourceHandler(this.getClass(), "/username-only/templates/login.hbs", "text/plain");
+        StaticResourceHandler lessHandler = new StaticResourceHandler(this.getClass(), "/username-only/less/login.less", "text/plain");
 
-        app.get("/js/configuration/plugins/authentication/authentication.js", jsHandler);
         app.get("/jsc/configuration/plugins/authentication/authentication.js", jsHandler);
         app.get("/jsc/configuration/plugins/authentication/templates/login.hbs", loginTemplateHandler);
+        app.get("/jsc/configuration/plugins/authentication/less/login.less", lessHandler);
+
         app.post(AuthenticationHandler.LOGIN_PATH, new Login(this.userRepository, this.workspaceRepository, this.configuration));
     }
 }
