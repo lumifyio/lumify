de12429dbc4ce5494382dcec10008646ea1ee3dc
Joe Ferner <joe@fernsroth.com>
2014-05-15 11:22:11 -0400

    pasting lat,lon into the lat field automatically splits [fixes #70933236]

--- lumify-web-war/src/main/webapp/js/fields/geoLocation.js
+++ lumify-web-war/src/main/webapp/js/fields/geoLocation.js
@@ -12,13 +12,30 @@ define([
         return parseFloat(v, 10);
     }
 
+    function splitLatLon(latLonStr) {
+        var parts = latLonStr.split(',');
+        if (parts.length == 2) {
+            return [ $.trim(parts[0]), $.trim(parts[1]) ];
+        }
+        return null;
+    }
+
     function GeoLocationField() {
 
         this.after('initialize', function() {
+            var _this = this;
             this.$node.html(template(this.attr));
 
             this.on('change keyup', {
                 inputSelector: function(event) {
+                    var latLon = splitLatLon(this.getValues()[0]);
+                    if (latLon) {
+                        var latInput = _this.$node.find('input.lat');
+                        var lonInput = _this.$node.find('input.lon');
+                        latInput.val(latLon[0]);
+                        lonInput.val(latLon[1]);
+                        lonInput.focus();
+                    }
                     this.filterUpdated(this.getValues().map(function(v) {
                         return makeNumber(v);
                     }));
