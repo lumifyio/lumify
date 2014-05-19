9edbd0914e041e9d2f8e9dd4b79de99242891cbe
Jeff Kunkle <jeff.kunkle@altamiracorp.com>
2014-05-19 13:32:19 -0400

    Basic X.509 identity authentication plugin

---
+++ lumify-web-auth-x509/src/main/java/io/lumify/web/auth/X509IdentityAuthenticationHandler.java
@@ -0,0 +1,7 @@
+package io.lumify.web.auth;
+
+import io.lumify.web.X509AuthenticationHandler;
+
+public class X509IdentityAuthenticationHandler extends X509AuthenticationHandler {
+    // default behavior is all that's needed
+}
