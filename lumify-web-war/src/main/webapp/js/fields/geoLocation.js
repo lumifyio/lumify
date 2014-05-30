fbefa7d86223c8699eb6eac73d175c633a9f421b
Jason Harwig <jason@pinepointsoftware.com>
2014-05-30 17:09:35 -0500

    Make geo dropdown same width as field

--- lumify-web-war/src/main/webapp/js/fields/geoLocation.js
+++ lumify-web-war/src/main/webapp/js/fields/geoLocation.js
@@ -77,6 +77,7 @@ define([
                             request;
 
                         self.select('descriptionSelector')
+                            .parent().css('position', 'relative').end()
                             .typeahead({
                                 items: 15,
                                 minLength: 3,
