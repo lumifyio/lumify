f3103f527aff268056a3bc0199046ca945af9169
Jason Harwig <jason@pinepointsoftware.com>
2014-06-03 13:21:19 -0500

    Add CSRF protection
    * Adds handler that protects non-get requests with session/request (header/parameter)
    * Adds clientside ajax prefilter that adds token to Lumify-CSRF-Token header
    
    [#70272464]

---
+++ lumify-web/src/main/java/io/lumify/web/LumifyCsrfHandler.java
@@ -0,0 +1,14 @@
+package io.lumify.web;
+
+import com.altamiracorp.miniweb.handlers.CSRFHandler;
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+public class LumifyCsrfHandler extends CSRFHandler {
+    public static final String PARAMETER_NAME = "csrfToken";
+    public static final String HEADER_NAME = "Lumify-CSRF-Token";
+
+    public LumifyCsrfHandler() {
+        super(PARAMETER_NAME, HEADER_NAME);
+    }
+}
