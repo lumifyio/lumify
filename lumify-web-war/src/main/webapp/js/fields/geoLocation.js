9a03548ad5ce4c3fb118d183e87e3bf7ae39b11a
Jason Harwig <jason@pinepointsoftware.com>
2014-06-02 19:01:59 -0500

    Add geo description typeahead to search filters if has geocoder

--- lumify-web-war/src/main/webapp/js/fields/geoLocation.js
+++ lumify-web-war/src/main/webapp/js/fields/geoLocation.js
@@ -29,92 +29,132 @@ define([
         });
 
         this.after('initialize', function() {
-            var self = this;
-            this.$node.html(template(this.attr));
+            var self = this,
+                d;
 
-            this.setupDescriptionTypeahead();
-            this.on('change keyup', {
-                inputSelector: function(event) {
-                    var latLon = splitLatLon(this.getValues()[1]);
-                    if (latLon) {
-                        var latInput = self.$node.find('input.lat'),
-                            lonInput = self.$node.find('input.lon');
-                        latInput.val(latLon[0]);
-                        lonInput.val(latLon[1]);
-                        lonInput.focus();
-                    }
+            if (this.attr.predicates) {
+                d = this.hasGeocoder().done(function(enabled) {
+                    self.attr.hasGeocoder = enabled;
+                });
+            } else {
+                d = $.Deferred().resolve();
+            }
 
-                    var values = this.getValues();
-                    this.filterUpdated(values.map(function(v, i) {
-                        if (values.length === 3 && i === 0) {
-                            return v;
+            d.done(function() {
+                self.$node.html(template(self.attr));
+                self.setupDescriptionTypeahead();
+                self.on(self.select('descriptionSelector'), 'focus blur', self.onFocusDescription);
+                self.on('change keyup', {
+                    inputSelector: function(event) {
+                        var latLon = splitLatLon(self.getValues()[1]);
+                        if (latLon) {
+                            var latInput = self.$node.find('input.lat'),
+                                lonInput = self.$node.find('input.lon');
+                            latInput.val(latLon[0]);
+                            lonInput.val(latLon[1]);
+                            lonInput.focus();
                         }
-                        return makeNumber(v);
-                    }));
-                }
-            });
+
+                        self.triggerUpdate();
+                    }
+                });
+            })
         });
 
-        this.isValid = function() {
+        this.onFocusDescription = function(event) {
+            if (!this.attr.predicates || !this.attr.hasGeocoder) {
+                return;
+            }
+
+            this.$node.toggleClass('desc-focus', event.type === 'focus');
+        }
+
+        this.triggerUpdate = function() {
             var values = this.getValues();
+            this.filterUpdated(_.compact(values.map(function(v, i) {
+                if (values.length === 3 && i === 0) {
+                    return v;
+                }
+                return makeNumber(v);
+            })));
+        }
+
+        this.isValid = function() {
+            var self = this,
+                values = this.getValues();
 
             return _.every(values, function(v, i) {
-                if (values.length === 3 && i === 0) {
+                if ((self.attr.hasGeocoder || !self.attr.predicates) && i === 0) {
                     return true;
+                }
+                if (self.attr.predicates && i === (self.attr.hasGeocoder ? 3 : 2)) {
+                    return makeNumber(v) > 0;
                 }
                 return v.length && _.isNumber(makeNumber(v)) && !isNaN(v);
             });
         };
 
+        this.hasGeocoder = function() {
+            return (new ConfigService()).getProperties()
+                .then(function(config) {
+                    return config['geocoder.enabled'] === 'true';
+                });
+        };
+
         this.setupDescriptionTypeahead = function() {
             var self = this;
 
-            (new ConfigService()).getProperties()
-                .done(function(config) {
-                    if (config['geocoder.enabled'] === 'true') {
-                        var mapService = new MapService(),
-                            savedResults,
-                            request;
+            this.hasGeocoder().done(function(enabled) {
+                if (enabled) {
+                    var mapService = new MapService(),
+                        savedResults,
+                        request;
 
-                        self.select('descriptionSelector')
-                            .parent().css('position', 'relative').end()
-                            .typeahead({
-                                items: 15,
-                                minLength: 3,
-                                source: function(q, process) {
-                                    if (request && request.abort) {
-                                        request.abort();
-                                    }
-
-                                    request = mapService.geocode(q)
-                                        .fail(function() {
-                                            process([]);
-                                        })
-                                        .done(function(data) {
-                                            savedResults = _.indexBy(data.results, 'name');
-                                            process(_.keys(savedResults));
-                                        });
-                                },
-                                updater: function(item) {
-                                    var result = savedResults[item];
-                                    if (result) {
-                                        var lat = self.$node.find('.lat').val(result.latitude)
-                                                .parent().removePrefixedClasses('pop-'),
-                                            lon = self.$node.find('.lon').val(result.longitude)
-                                                .parent().removePrefixedClasses('pop-');
-
-                                        requestAnimationFrame(function() {
-                                            lat.addClass('pop-fast');
-                                            _.delay(function() {
-                                                lon.addClass('pop-fast');
-                                            }, 250)
-                                        })
-
-                                        return result.name;
-                                    }
-                                    return item;
+                    self.select('descriptionSelector')
+                        .parent().css('position', 'relative').end()
+                        .typeahead({
+                            items: 15,
+                            minLength: 3,
+                            source: function(q, process) {
+                                if (request && request.abort) {
+                                    request.abort();
                                 }
-                            });
+
+                                request = mapService.geocode(q)
+                                    .fail(function() {
+                                        process([]);
+                                    })
+                                    .done(function(data) {
+                                        savedResults = _.indexBy(data.results, 'name');
+                                        process(_.keys(savedResults));
+                                    });
+                            },
+                            updater: function(item) {
+                                var result = savedResults[item];
+                                if (result) {
+                                    var lat = self.$node.find('.lat').val(result.latitude)
+                                            .parent().removePrefixedClasses('pop-'),
+                                        lon = self.$node.find('.lon').val(result.longitude)
+                                            .parent().removePrefixedClasses('pop-');
+
+                                    requestAnimationFrame(function() {
+                                        lat.addClass('pop-fast');
+                                        _.delay(function() {
+                                            lon.addClass('pop-fast');
+                                        }, 250)
+                                    })
+
+                                    if (self.isValid()) {
+                                        self.triggerUpdate();
+                                    } else {
+                                        self.$node.find('.radius').focus();
+                                    }
+
+                                    return result.name;
+                                }
+                                return item;
+                            }
+                        });
                     }
                 });
         }
