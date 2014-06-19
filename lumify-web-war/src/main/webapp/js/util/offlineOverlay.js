0d58abf1a739e1dd9875a1f3a11349ad4abf7d2f
Jason Harwig <jason@pinepointsoftware.com>
2014-06-19 14:24:58 -0500

    When server goes down, just display an overlay instead of showing login screen

---
+++ lumify-web-war/src/main/webapp/js/util/offlineOverlay.js
@@ -0,0 +1,56 @@
+define([
+    'flight/lib/component',
+    'hbs!./offlineOverlayTpl',
+    'util/formatters'
+], function(
+    defineComponent,
+    template,
+    F) {
+    'use strict';
+
+    return defineComponent(Overlay);
+
+    function Overlay() {
+
+        this.after('teardown', function() {
+            this.overlay.remove();
+        });
+
+        this.after('initialize', function() {
+            var self = this;
+            $(function() {
+                self.overlay = $(template())
+                    .appendTo(document.body);
+
+                self.$lastCheck = self.overlay.find('.last-checked');
+                setInterval(self.updateLastCheck.bind(self), 30000)
+                self.poll(1);
+                self.updateLastCheck();
+
+            });
+        });
+
+        this.updateLastCheck = function() {
+            if (this.lastCheck) {
+                this.$lastCheck.text('Last checked ' + F.date.relativeToNow(this.lastCheck)).show();
+            } else {
+                this.$lastCheck.hide();
+            }
+        };
+
+        this.poll = function(checkNumber) {
+            var self = this;
+
+            this.lastCheck = F.date.utc(new Date()).getTime();
+            $.get('ping.html')
+                .done(function() {
+                    window.location.reload();
+                })
+                .fail(function() {
+                    var waitTimeSeconds = Math.min(120, Math.pow(2, checkNumber) - 1);
+                    setTimeout(self.poll.bind(self, checkNumber + 0.5), waitTimeSeconds * 1000);
+                });
+        }
+
+    }
+});
