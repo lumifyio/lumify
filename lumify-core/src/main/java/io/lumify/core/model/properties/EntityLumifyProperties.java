8c7597b735a13d448a21edd3689440e1d50ccaac
Joe Ferner <joe@fernsroth.com>
2014-06-26 11:24:28 -0400

    add source url to backend

--- lumify-core/src/main/java/io/lumify/core/model/properties/EntityLumifyProperties.java
+++ lumify-core/src/main/java/io/lumify/core/model/properties/EntityLumifyProperties.java
@@ -1,13 +1,13 @@
 package io.lumify.core.model.properties;
 
 import io.lumify.core.model.properties.types.StringLumifyProperty;
-import org.securegraph.TextIndexHint;
 
 /**
  * LumifyProperties that apply to both raw and resolved entities stored in the Lumify system.
  */
 public class EntityLumifyProperties {
     public static final StringLumifyProperty SOURCE = new StringLumifyProperty("http://lumify.io#source");
+    public static final StringLumifyProperty SOURCE_URL = new StringLumifyProperty("http://lumify.io#sourceUrl");
     public static final StringLumifyProperty IMAGE_VERTEX_ID = new StringLumifyProperty("http://lumify.io#entityImageVertexId");
 
     private EntityLumifyProperties() {
