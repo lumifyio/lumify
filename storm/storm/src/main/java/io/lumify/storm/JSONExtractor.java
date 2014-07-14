064963db478961ffef40de995f751504fc86474c
Jonathon Hellmann <jonatho7@vt.edu>
2014-07-14 17:01:31 -0400

    added JSONExtractor class

--- storm/storm/src/main/java/io/lumify/storm/JSONExtractor.java
+++ storm/storm/src/main/java/io/lumify/storm/JSONExtractor.java
@@ -1,4 +1,35 @@
 package io.lumify.storm;
 
+import io.lumify.core.ingest.graphProperty.GraphPropertyWorkData;
+import io.lumify.core.util.LumifyLogger;
+import io.lumify.core.util.LumifyLoggerFactory;
+import io.lumify.core.util.ProcessRunner;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+
 public class JSONExtractor {
+    private static final LumifyLogger LOGGER = LumifyLoggerFactory.getLogger(JSONExtractor.class);
+
+    public static JSONObject retrieveJSONObjectUsingFFPROBE(ProcessRunner processRunner, GraphPropertyWorkData data)
+            throws Exception {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        processRunner.execute(
+                "ffprobe",
+                new String[]{
+                        "-v", "quiet",
+                        "-print_format", "json",
+                        "-show_format",
+                        "-show_streams",
+                        data.getLocalFile().getAbsolutePath()
+                },
+                out,
+                data.getLocalFile().getAbsolutePath() + ": "
+        );
+        String outString = new String(out.toByteArray());
+        JSONObject json = new JSONObject(outString);
+        LOGGER.debug("info for %s:\n%s", data.getLocalFile().getAbsolutePath(), json.toString(2));
+        return json;
+    }
 }
