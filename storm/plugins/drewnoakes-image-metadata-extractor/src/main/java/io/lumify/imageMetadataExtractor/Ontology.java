489b71f834b2fadf4024d6b3a6cf0d37cf19d38b
Jonathon Hellmann <jonatho7@vt.edu>
2014-07-17 13:24:04 -0400

    added camelCase for Ontology variables

--- storm/plugins/drewnoakes-image-metadata-extractor/src/main/java/io/lumify/imageMetadataExtractor/Ontology.java
+++ storm/plugins/drewnoakes-image-metadata-extractor/src/main/java/io/lumify/imageMetadataExtractor/Ontology.java
@@ -6,9 +6,9 @@ public class Ontology {
 
     public static final IntegerLumifyProperty CW_ROTATION_NEEDED = new IntegerLumifyProperty("http://lumify.io/exif#cwRotationNeeded");
     public static final BooleanLumifyProperty Y_AXIS_FLIP_NEEDED = new BooleanLumifyProperty("http://lumify.io/exif#yAxisFlipNeeded");
-    public static final DateLumifyProperty DATE_TAKEN = new DateLumifyProperty("http://lumify.io/exif#datetaken");
-    public static final StringLumifyProperty DEVICE_MAKE = new StringLumifyProperty("http://lumify.io/exif#devicemake");
-    public static final StringLumifyProperty DEVICE_MODEL = new StringLumifyProperty("http://lumify.io/exif#devicemodel");
+    public static final DateLumifyProperty DATE_TAKEN = new DateLumifyProperty("http://lumify.io/exif#dateTaken");
+    public static final StringLumifyProperty DEVICE_MAKE = new StringLumifyProperty("http://lumify.io/exif#deviceMake");
+    public static final StringLumifyProperty DEVICE_MODEL = new StringLumifyProperty("http://lumify.io/exif#deviceModel");
     public static final GeoPointLumifyProperty GEO_LOCATION = new GeoPointLumifyProperty("http://lumify.io/exif#geoLocation");
     public static final DoubleLumifyProperty DIRECTION = new DoubleLumifyProperty("http://lumify.io/exif#direction");
     public static final StringLumifyProperty DIRECTION_DESCRIPTION = new StringLumifyProperty("http://lumify.io/exif#directionDescription");
