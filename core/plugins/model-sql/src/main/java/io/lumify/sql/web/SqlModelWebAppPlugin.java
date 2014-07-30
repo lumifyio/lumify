a1442e548b682b24b37ce443096ba9c985488971
Jeff Kunkle <jeff.kunkle@altamiracorp.com>
2014-07-30 15:05:12 -0400

    Automatically register hibernate session management filter

--- core/plugins/model-sql/src/main/java/io/lumify/sql/web/SqlModelWebAppPlugin.java
+++ core/plugins/model-sql/src/main/java/io/lumify/sql/web/SqlModelWebAppPlugin.java
@@ -4,19 +4,27 @@ import com.altamiracorp.miniweb.Handler;
 import com.google.inject.Inject;
 import io.lumify.sql.model.HibernateSessionManager;
 import io.lumify.web.WebApp;
+import io.lumify.web.WebAppInitializer;
 import io.lumify.web.WebAppPlugin;
 import org.hibernate.SessionFactory;
 
-import javax.servlet.ServletConfig;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import java.util.EnumSet;
 
 public class SqlModelWebAppPlugin implements WebAppPlugin {
+    public static final String FILTER_NAME = "hibernate-session-manager";
+
     @Inject
     public void configure(SessionFactory sessionFactory) {
         HibernateSessionManager.initialize(sessionFactory);
     }
 
     @Override
-    public void init(WebApp app, ServletConfig config, Handler authenticationHandler) {
-        // nothing to do
+    public void init(WebApp app, ServletContext servletContext, Handler authenticationHandler) {
+        FilterRegistration.Dynamic filter = servletContext.addFilter(FILTER_NAME, new HibernateSessionManagementFilter());
+        filter.addMappingForServletNames(EnumSet.of(DispatcherType.REQUEST), false, WebAppInitializer.SERVLET_NAME);
+        filter.setAsyncSupported(true);
     }
 }
--- web/web-base/src/main/java/io/lumify/web/WebAppInitializer.java
+++ web/web-base/src/main/java/io/lumify/web/WebAppInitializer.java
@@ -6,11 +6,13 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.ServletRegistration;
 
 public class WebAppInitializer implements ServletContextListener {
+    public static final String SERVLET_NAME = "lumify";
+
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         ServletContext servletContext = servletContextEvent.getServletContext();
         Router router = new Router(servletContext);
-        ServletRegistration.Dynamic servlet = servletContext.addServlet("lumify", router);
+        ServletRegistration.Dynamic servlet = servletContext.addServlet(SERVLET_NAME, router);
         servlet.addMapping("/*");
         servlet.setAsyncSupported(true);
     }
