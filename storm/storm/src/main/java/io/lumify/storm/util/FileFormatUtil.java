ed46b087bd3bc83d4c792837f1e19a5c65d0a055
Jonathon Hellmann <jonatho7@vt.edu>
2014-08-12 19:24:33 -0400

    FileFormatUtil - extract fileFormat

--- storm/storm/src/main/java/io/lumify/storm/util/FileFormatUtil.java
+++ storm/storm/src/main/java/io/lumify/storm/util/FileFormatUtil.java
@@ -15,18 +15,24 @@ public class FileFormatUtil {
         JSONObject formatObject = json.optJSONObject("format");
         if (formatObject != null) {
             String optionalFileName = formatObject.optString("filename");
-            if (!optionalFileName.equals("")){
+            if (!optionalFileName.equals("")) {
 
                 /*Take the last characters of the filename (before the graphPropertyBolt number) as the file format.
-                    and do not take more than 4 characters as the filename.
+                    File format must start with a letter, and may be 3 or 4 characters. Ex: webm, mp4, mov, divx.
                  */
-                int numberPosition = optionalFileName.length() - 1;
-                for(int i = optionalFileName.length(); i > 0 && (i > optionalFileName.length() - 5 ); i--){
-                    String oneCharacter = optionalFileName.substring(i - 1, i);
-                    if (oneCharacter != null && isNumeric(oneCharacter) ){
-                        numberPosition = i - 1;
-                        String fileEnding = optionalFileName.substring(numberPosition + 1, optionalFileName.length());
-                        return fileEnding;
+                if (optionalFileName.length() >= 4) {
+                    String characterFour = optionalFileName.substring(optionalFileName.length() - 4, optionalFileName.length() - 3);
+                    if (!isNumeric(characterFour)) {
+                        String lastFourCharacters = optionalFileName.substring(optionalFileName.length() - 4, optionalFileName.length());
+                        return lastFourCharacters;
+                    }
+                }
+
+                if (optionalFileName.length() >= 3) {
+                    String characterThree = optionalFileName.substring(optionalFileName.length() - 3, optionalFileName.length() - 2);
+                    if (!isNumeric(characterThree)) {
+                        String lastThreeCharacters = optionalFileName.substring(optionalFileName.length() - 3, optionalFileName.length());
+                        return lastThreeCharacters;
                     }
                 }
             }
@@ -36,14 +42,10 @@ public class FileFormatUtil {
         return null;
     }
 
-    private static boolean isNumeric(String inputString)
-    {
-        try
-        {
+    private static boolean isNumeric(String inputString) {
+        try {
             int temp = Integer.parseInt(inputString);
-        }
-        catch(NumberFormatException nfe)
-        {
+        } catch (NumberFormatException nfe) {
             return false;
         }
         return true;
