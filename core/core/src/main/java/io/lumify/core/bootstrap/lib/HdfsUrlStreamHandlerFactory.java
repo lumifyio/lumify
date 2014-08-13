a356a725e7ad8faf11e3b7607f21faa33db48d89
Jeff Kunkle <jeff.kunkle@altamiracorp.com>
2014-08-13 11:38:08 -0400

    Only support hdfs explicitly

--- core/core/src/main/java/io/lumify/core/bootstrap/lib/HdfsUrlStreamHandlerFactory.java
+++ core/core/src/main/java/io/lumify/core/bootstrap/lib/HdfsUrlStreamHandlerFactory.java
@@ -1,45 +1,22 @@
 package io.lumify.core.bootstrap.lib;
 
-import java.net.URLStreamHandlerFactory;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.hadoop.conf.Configuration;
 
-/**
- * The code for this class was copied form org.apache.hadoop.fs.FsUrlStreamHandlerFactory
- * and adapted to allow for passing the HDFS user.
- */
+import java.net.URLStreamHandlerFactory;
+
 public class HdfsUrlStreamHandlerFactory implements URLStreamHandlerFactory {
 
-    // The configuration holds supported FS implementation class names.
-    private Configuration conf;
-
-    // This map stores whether a protocol is know or not by FileSystem
-    private Map<String, Boolean> protocols = new HashMap<String, Boolean>();
-
-    // The URL Stream handler
     private java.net.URLStreamHandler handler;
 
     public HdfsUrlStreamHandlerFactory(Configuration conf, String user) {
-        this.conf = new Configuration(conf);
-        // force the resolution of the configuration files
-        this.conf.getClass("fs.file.impl", null);
-        this.handler = new HdfsUrlStreamHandler(this.conf, user);
+        this.handler = new HdfsUrlStreamHandler(conf, user);
     }
 
     public java.net.URLStreamHandler createURLStreamHandler(String protocol) {
-        if (!protocols.containsKey(protocol)) {
-            boolean known =
-                    (conf.getClass("fs." + protocol + ".impl", null) != null);
-            protocols.put(protocol, known);
-        }
-        if (protocols.get(protocol)) {
+        if (protocol.toLowerCase().equals("hdfs")) {
             return handler;
-        } else {
-            // FileSystem does not know the protocol, let the VM handle this
-            return null;
         }
+        return null;
     }
 
 }
