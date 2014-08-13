aa4d4d3d385615161ef40d4d4d25b57d062544f2
Jonathon Hellmann <jonatho7@vt.edu>
2014-08-13 15:27:31 -0400

    FileSizeUtil class implemented

--- storm/storm/src/main/java/io/lumify/storm/util/FileSizeUtil.java
+++ storm/storm/src/main/java/io/lumify/storm/util/FileSizeUtil.java
@@ -1,27 +1,16 @@
 package io.lumify.storm.util;
 
-import io.lumify.core.util.LumifyLogger;
-import io.lumify.core.util.LumifyLoggerFactory;
-import org.json.JSONObject;
+import java.io.File;
 
 public class FileSizeUtil {
-    private static final LumifyLogger LOGGER = LumifyLoggerFactory.getLogger(FileSizeUtil.class);
 
-    public static Integer extractFileSizeFromJSON(JSONObject json) {
-        if (json == null) {
-            return null;
+    public static Integer extractFileSize(File file){
+        Long fileSizeLong = file.length();
+        if (fileSizeLong != null && fileSizeLong > 0 && fileSizeLong < Integer.MAX_VALUE) {
+            Integer fileSize = fileSizeLong.intValue();
+            return fileSize;
         }
 
-        JSONObject formatObject = json.optJSONObject("format");
-        if (formatObject != null) {
-            Double optionalFileSize = formatObject.optDouble("size");
-            if (!Double.isNaN(optionalFileSize)) {
-                Integer fileSize = optionalFileSize.intValue();
-                return fileSize;
-            }
-        }
-
-        LOGGER.debug("Could not extract fileSize from json.");
         return null;
     }
 }
