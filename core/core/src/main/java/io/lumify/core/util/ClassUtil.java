a9f9f179e9f4714fc9f7f4f9d3af54de23f2d3bb
David Singley <dsingley@nearinfinity.com>
2014-08-20 18:30:08 -0400

    new method to debug classpath

--- core/core/src/main/java/io/lumify/core/util/ClassUtil.java
+++ core/core/src/main/java/io/lumify/core/util/ClassUtil.java
@@ -3,6 +3,8 @@ package io.lumify.core.util;
 import io.lumify.core.exception.LumifyException;
 
 import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 public class ClassUtil {
     @SuppressWarnings("unchecked")
@@ -25,8 +27,36 @@ public class ClassUtil {
         return null;
     }
 
+    public static void logClasspath(ClassLoader classLoader) {
+        logClasspath(classLoader, null);
+    }
+
+    public static void logClasspath(ClassLoader classLoader, LumifyLogger lumifyLogger) {
+        if (classLoader instanceof URLClassLoader) {
+            log(lumifyLogger, classLoader.getClass().getName());
+            for (URL url : ((URLClassLoader) classLoader).getURLs()) {
+                log(lumifyLogger, url.toString());
+            }
+        } else {
+            log(lumifyLogger, "unable to enumerate entries for " + classLoader.getClass().getName());
+        }
+
+        ClassLoader parentClassLoader = classLoader.getParent();
+        if (parentClassLoader != null) {
+            logClasspath(parentClassLoader, lumifyLogger);
+        }
+    }
+
     @SuppressWarnings("unchecked")
     private static Method getDeclaredMethod(Class clazz, String methodName, Class[] parameters) throws NoSuchMethodException {
         return clazz.getDeclaredMethod(methodName, parameters);
+    }
+
+    private static void log(LumifyLogger lumifyLogger, String string) {
+        if (lumifyLogger != null) {
+            lumifyLogger.debug(string);
+        } else {
+            System.out.println(string);
+        }
     }
 }
