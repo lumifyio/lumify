df5412c6ad400413d9ab701db1fa3c14147a6d2b
Joe Ferner <joe@fernsroth.com>
2014-08-25 16:01:50 -0400

    possible fix shortlived processes getting the right output

--- core/core/src/main/java/io/lumify/core/util/Pipe.java
+++ core/core/src/main/java/io/lumify/core/util/Pipe.java
@@ -1,5 +1,7 @@
 package io.lumify.core.util;
 
+import io.lumify.core.exception.LumifyException;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -13,13 +15,17 @@ public class Pipe {
     private Semaphore completionSemaphore = new Semaphore(1);
 
     public Pipe pipe(final InputStream in, final OutputStream out, final StatusHandler statusHandler) {
+        final boolean[] threadStarted = new boolean[1];
+        threadStarted[0] = false;
+
         executor.execute(new Runnable() {
             @Override
             public void run() {
-                int read;
-                byte[] buffer = new byte[1 * 1024 * 1024];
                 try {
+                    int read;
+                    byte[] buffer = new byte[1 * 1024 * 1024];
                     completionSemaphore.acquire();
+                    threadStarted[0] = true;
                     while ((read = in.read(buffer)) > 0) {
                         out.write(buffer, 0, read);
                     }
@@ -28,11 +34,23 @@ public class Pipe {
                 } catch (InterruptedException e) {
                     statusHandler.handleException(e);
                 } finally {
+                    threadStarted[0] = true; // if something fails before we get a chance to set this to true make sure it happens
                     statusHandler.handleComplete();
                     completionSemaphore.release();
                 }
             }
         });
+
+        // It could be possible for the process to exit before this thread gets started resulting in this thread
+        //   to not read the output stream. This waiting is to make sure the thread is started before we return.
+        //   http://stackoverflow.com/questions/2150723/process-waitfor-threads-and-inputstreams (see resolution)
+        while (!threadStarted[0]) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new LumifyException("Could not sleep", e);
+            }
+        }
         return this;
     }
 
