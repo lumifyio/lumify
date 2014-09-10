f8983eb101765cf1677a9fbcf8c56316a6b2c3ed
Susan Feng <susan.feng@altamiracorp.com>
2014-09-10 11:35:15 -0400

    add term mention visibility to im memory auth

--- storm/plugins/opennlp-me-extractor/src/test/java/io/lumify/opennlpme/OpenNLPMaximumEntropyExtractorGraphPropertyWorkerTest.java
+++ storm/plugins/opennlp-me-extractor/src/test/java/io/lumify/opennlpme/OpenNLPMaximumEntropyExtractorGraphPropertyWorkerTest.java
@@ -6,6 +6,7 @@ import io.lumify.core.ingest.graphProperty.GraphPropertyWorkData;
 import io.lumify.core.ingest.graphProperty.GraphPropertyWorkerPrepareData;
 import io.lumify.core.ingest.graphProperty.TermMentionFilter;
 import io.lumify.core.model.properties.LumifyProperties;
+import io.lumify.core.model.termMention.TermMentionRepository;
 import io.lumify.core.security.DirectVisibilityTranslator;
 import io.lumify.core.security.VisibilityTranslator;
 import io.lumify.core.user.User;
@@ -71,7 +72,7 @@ public class OpenNLPMaximumEntropyExtractorGraphPropertyWorkerTest {
         stormConf.put(OpenNLPMaximumEntropyExtractorGraphPropertyWorker.PATH_PREFIX_CONFIG, "file:///" + getClass().getResource(RESOURCE_CONFIG_DIR).getFile());
 
         FileSystem hdfsFileSystem = FileSystem.get(new Configuration());
-        authorizations = new InMemoryAuthorizations();
+        authorizations = new InMemoryAuthorizations(TermMentionRepository.VISIBILITY);
         Injector injector = null;
         List<TermMentionFilter> termMentionFilters = new ArrayList<TermMentionFilter>();
         GraphPropertyWorkerPrepareData workerPrepareData = new GraphPropertyWorkerPrepareData(stormConf, termMentionFilters, hdfsFileSystem, user, authorizations, injector);
