7abbb4503879c8e0600fcd436815b863d13efeae
Jeff Kunkle <jeff.kunkle@altamiracorp.com>
2014-09-19 15:41:31 -0400

    Move webapp initialization to ApplicationBootstrap class

--- web/web-base/src/main/java/io/lumify/web/ApplicationBootstrap.java
+++ web/web-base/src/main/java/io/lumify/web/ApplicationBootstrap.java
@@ -7,7 +7,6 @@ import io.lumify.core.bootstrap.InjectHelper;
 import io.lumify.core.bootstrap.LumifyBootstrap;
 import io.lumify.core.config.Configuration;
 import io.lumify.core.config.ConfigurationLoader;
-import io.lumify.core.config.FileConfigurationLoader;
 import io.lumify.core.model.ontology.OntologyRepository;
 import io.lumify.core.model.user.UserRepository;
 import io.lumify.core.util.LumifyLogger;
@@ -16,6 +15,7 @@ import io.lumify.core.util.LumifyLoggerFactory;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRegistration;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
@@ -23,38 +23,23 @@ import java.util.Map;
 public final class ApplicationBootstrap implements ServletContextListener {
     private static LumifyLogger LOGGER;
     public static final String APP_CONFIG_LOADER = "application.config.loader";
+    public static final String LUMIFY_SERVLET_NAME = "lumify";
     private UserRepository userRepository;
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         final ServletContext context = sce.getServletContext();
-
         System.out.println("Servlet context initialized...");
-        try {
-            if (context != null) {
-                final Configuration config = ConfigurationLoader.load(context.getInitParameter(APP_CONFIG_LOADER), getInitParametersAsMap(context));
-                LOGGER = LumifyLoggerFactory.getLogger(ApplicationBootstrap.class);
-                LOGGER.info("Running application with configuration:\n%s", config);
 
-                InjectHelper.inject(this, LumifyBootstrap.bootstrapModuleMaker(config));
+        if (context != null) {
+            final Configuration config = ConfigurationLoader.load(context.getInitParameter(APP_CONFIG_LOADER), getInitParametersAsMap(context));
+            LOGGER = LumifyLoggerFactory.getLogger(ApplicationBootstrap.class);
+            LOGGER.info("Running application with configuration:\n%s", config);
 
-                // Store the injector in the context for a servlet to access later
-                context.setAttribute(Injector.class.getName(), InjectHelper.getInjector());
-                if (!config.get(Configuration.MODEL_PROVIDER).equals(Configuration.UNKNOWN_STRING)) {
-                    FrameworkUtils.initializeFramework(InjectHelper.getInjector(), userRepository.getSystemUser());
-                }
-
-                InjectHelper.getInjector().getInstance(OntologyRepository.class);
-
-                LOGGER.warn("JavaScript / Less modifications will not be reflected on server. Run `grunt watch` from webapp directory in development");
-            } else {
-                System.out.println("Servlet context could not be acquired!");
-            }
-        } catch (Exception ex) {
-            if (LOGGER != null) {
-                LOGGER.error("Failed to initialize context", ex);
-            }
-            throw new RuntimeException("Failed to initialize context", ex);
+            setupInjector(context, config);
+            setupWebApp(context);
+        } else {
+            throw new RuntimeException("Failed to initialize context. Lumify is not running.");
         }
     }
 
@@ -65,6 +50,31 @@ public final class ApplicationBootstrap implements ServletContextListener {
         }
     }
 
+    @Inject
+    public void setUserRepository(UserRepository userProvider) {
+        this.userRepository = userProvider;
+    }
+
+    private void setupInjector(ServletContext context, Configuration config) {
+        InjectHelper.inject(this, LumifyBootstrap.bootstrapModuleMaker(config));
+
+        // Store the injector in the context for a servlet to access later
+        context.setAttribute(Injector.class.getName(), InjectHelper.getInjector());
+        if (!config.get(Configuration.MODEL_PROVIDER).equals(Configuration.UNKNOWN_STRING)) {
+            FrameworkUtils.initializeFramework(InjectHelper.getInjector(), userRepository.getSystemUser());
+        }
+
+        InjectHelper.getInjector().getInstance(OntologyRepository.class);
+    }
+
+    private void setupWebApp(ServletContext context) {
+        Router router = new Router(context);
+        ServletRegistration.Dynamic servlet = context.addServlet(LUMIFY_SERVLET_NAME, router);
+        servlet.addMapping("/*");
+        servlet.setAsyncSupported(true);
+        LOGGER.warn("JavaScript / Less modifications will not be reflected on server. Run `grunt watch` from webapp directory in development");
+    }
+
     private Map<String, String> getInitParametersAsMap(ServletContext context) {
         Map<String, String> initParameters = new HashMap<String, String>();
         Enumeration<String> e = context.getInitParameterNames();
@@ -73,10 +83,5 @@ public final class ApplicationBootstrap implements ServletContextListener {
             initParameters.put(initParameterName, context.getInitParameter(initParameterName));
         }
         return initParameters;
-    }
-
-    @Inject
-    public void setUserRepository(UserRepository userProvider) {
-        this.userRepository = userProvider;
     }
 }
