b3e16e715d707194b50a5635c65eacf12a203772
Susan Feng <susan.feng@altamiracorp.com>
2014-09-19 15:57:24 -0400

    fix adding audit visibility to authorizations

--- core/core/src/main/java/io/lumify/core/model/audit/AuditRepository.java
+++ core/core/src/main/java/io/lumify/core/model/audit/AuditRepository.java
@@ -2,15 +2,12 @@ package io.lumify.core.model.audit;
 
 import com.google.inject.Inject;
 import io.lumify.core.model.user.AuthorizationRepository;
-import io.lumify.core.security.VisibilityTranslator;
-import io.lumify.core.version.VersionService;
-import org.securegraph.Graph;
 
 public class AuditRepository {
     public static final String AUDIT_VISIBILITY = "audit";
 
     @Inject
-    public AuditRepository() {
+    public AuditRepository(AuthorizationRepository authorizationRepository) {
         authorizationRepository.addAuthorizationToGraph(AUDIT_VISIBILITY);
     }
 
