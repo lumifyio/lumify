c013dd8b846874f394f5b7a106247ec940830f0f
David Singley <dsingley@nearinfinity.com>
2014-10-22 17:57:51 -0400

    display export menu option for each configured version

--- web/plugins/analysts-notebook-export/src/main/resources/io/lumify/analystsNotebook/analystsNotebook.js
+++ web/plugins/analysts-notebook-export/src/main/resources/io/lumify/analystsNotebook/analystsNotebook.js
@@ -1,12 +1,31 @@
 require([
     'configuration/plugins/exportWorkspace/plugin',
-    'util/messages'
-], function(p, i18n) {
-    var componentPath = 'io.lumify.analystsNotebook.AnalystsNotebook';
+    'util/messages',
+    'service/config'
+], function(p, i18n, ConfigService) {
+    var componentPath = 'io.lumify.analystsNotebook.AnalystsNotebook',
+        configService = new ConfigService();
 
-    p.registerWorkspaceExporter({
-        menuItem: i18n('analystsnotebook.menuitem.title'),
-        componentPath: componentPath
+    configService.getProperties().done(function(config) {
+        var versions = {};
+
+        Object.getOwnPropertyNames(config).forEach(function(key) {
+            var labelMatch = key.match(/analystsNotebookExport\.menuOption\.(\w+)\.label/);
+            if (labelMatch && labelMatch.length == 2) {
+                var label = config["analystsNotebookExport.menuOption." + labelMatch[1] + ".label"],
+                    value = config["analystsNotebookExport.menuOption." + labelMatch[1] + ".value"];
+                if (label && value) {
+                    versions[label] = value;
+                }
+            }
+        });
+
+        Object.getOwnPropertyNames(versions).forEach(function(label) {
+            p.registerWorkspaceExporter({
+                menuItem: i18n('analystsnotebook.menuitem.title') + " " + label,
+                componentPath: componentPath
+            });
+        });
     });
 
     define(componentPath, ['flight/lib/component'], function(defineComponent) {
