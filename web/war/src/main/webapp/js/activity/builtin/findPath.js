0ea6f1663e0b785ba77413ec3a04f8657bf1569d
Jason Harwig <jason@pinepointsoftware.com>
2014-10-24 17:00:05 -0500

    Show how many vertices will be added

--- web/war/src/main/webapp/js/activity/builtin/findPath.js
+++ web/war/src/main/webapp/js/activity/builtin/findPath.js
@@ -1,14 +1,24 @@
 define([
-    'flight/lib/component'
+    'flight/lib/component',
+    'service/longRunningProcess'
 ], function(
-    defineComponent) {
+    defineComponent,
+    LongRunningProcessService) {
     'use strict';
+
+    var longRunningProcessService = new LongRunningProcessService();
 
     return defineComponent(FindPath);
 
     function FindPath() {
+        this.after('teardown', function() {
+            this.$node.empty();
+        });
+
         this.after('initialize', function() {
-            this.$node.html('<button class="btn btn-mini btn-primary">Add Vertices</button>');
+            if (_.isNumber(this.attr.process.resultsCount)) {
+                this.$node.html('<button class="btn btn-mini btn-primary">Add ' + this.attr.process.resultsCount + ' Vertices</button>');
+            }
         });
     }
 });
