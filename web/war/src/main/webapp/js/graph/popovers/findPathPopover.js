80da0d07fdd29998a51ce1302be947a93611c60f
Jason Harwig <jason@pinepointsoftware.com>
2014-10-24 13:10:51 -0500

    Show progress bars, X button deletes process

--- web/war/src/main/webapp/js/graph/popovers/findPathPopover.js
+++ web/war/src/main/webapp/js/graph/popovers/findPathPopover.js
@@ -66,60 +66,60 @@ define([
                     self.popover.find('span.path').text(i18n('popovers.find_path.error'));
                     self.positionDialog();
                 })
-                .done(function(result) {
+                //.done(function(result) {
 
-                    var paths = result.paths,
-                        vertices = result.uniqueVertices,
-                        verticesNotSourceDest = vertices.filter(function(v) {
-                            return v.id !== src && v.id !== dest;
-                        }),
-                        notInWorkspace = vertices.filter(function(v) {
-                            return !appData.workspaceVertices[v.id];
-                        }),
-                        pathsFoundText = i18n('popovers.find_path.found.' + (
-                            paths.length === 0 ? 'none' :
-                            paths.length === 1 ? 'one' : 'some'
-                        ), paths.length);
+                    //var paths = result.paths,
+                        //vertices = result.uniqueVertices,
+                        //verticesNotSourceDest = vertices.filter(function(v) {
+                            //return v.id !== src && v.id !== dest;
+                        //}),
+                        //notInWorkspace = vertices.filter(function(v) {
+                            //return !appData.workspaceVertices[v.id];
+                        //}),
+                        //pathsFoundText = i18n('popovers.find_path.found.' + (
+                            //paths.length === 0 ? 'none' :
+                            //paths.length === 1 ? 'one' : 'some'
+                        //), paths.length);
 
-                    if (paths.length) {
-                        if (notInWorkspace.length) {
-                            var vertexText = i18n('popovers.find_path.not_in_workspace.' + (
-                                    notInWorkspace.length === 1 ? 'one' : 'some'
-                                ), notInWorkspace.length);
+                    //if (paths.length) {
+                        //if (notInWorkspace.length) {
+                            //var vertexText = i18n('popovers.find_path.not_in_workspace.' + (
+                                    //notInWorkspace.length === 1 ? 'one' : 'some'
+                                //), notInWorkspace.length);
 
-                            text.text(vertexText);
-                            button.text(i18n('popovers.find_path.button.add.' + (
-                                notInWorkspace.length === 1 ? 'one' : 'some'
-                            ), notInWorkspace.length)).removeAttr('disabled').show();
+                            //text.text(vertexText);
+                            //button.text(i18n('popovers.find_path.button.add.' + (
+                                //notInWorkspace.length === 1 ? 'one' : 'some'
+                            //), notInWorkspace.length)).removeAttr('disabled').show();
 
-                            var index, map = {};
-                            for (var i = 0; i < notInWorkspace.length; i++) {
-                                pathLoop: for (var j = 0; j < paths.length; j++) {
-                                    for (var x = 0; x < paths[j].length; x++) {
-                                        if (paths[j][x].id === notInWorkspace[i].id) {
-                                            map[notInWorkspace[i].id] = {
-                                                sourceId: paths[j][x - 1].id,
-                                                targetId: paths[j][x + 1].id
-                                            };
-                                            break pathLoop;
-                                        }
-                                    }
-                                }
-                            }
+                            //var index, map = {};
+                            //for (var i = 0; i < notInWorkspace.length; i++) {
+                                //pathLoop: for (var j = 0; j < paths.length; j++) {
+                                    //for (var x = 0; x < paths[j].length; x++) {
+                                        //if (paths[j][x].id === notInWorkspace[i].id) {
+                                            //map[notInWorkspace[i].id] = {
+                                                //sourceId: paths[j][x - 1].id,
+                                                //targetId: paths[j][x + 1].id
+                                            //};
+                                            //break pathLoop;
+                                        //}
+                                    //}
+                                //}
+                            //}
 
-                            self.verticesToAdd = notInWorkspace;
-                            self.verticesToAddLayoutMap = map;
-                        } else {
-                            text.text(i18n('popovers.find_path.not_in_workspace.none'));
-                        }
+                            //self.verticesToAdd = notInWorkspace;
+                            //self.verticesToAddLayoutMap = map;
+                        //} else {
+                            //text.text(i18n('popovers.find_path.not_in_workspace.none'));
+                        //}
 
-                        cy.$('.temp').remove();
-                        self.trigger('focusPaths', { paths: paths, sourceId: src, targetId: dest });
-                    } else text.text(i18n('popovers.find_path.searching.hops', self.attr.hops));
+                        //cy.$('.temp').remove();
+                        //self.trigger('focusPaths', { paths: paths, sourceId: src, targetId: dest });
+                    //} else text.text(i18n('popovers.find_path.searching.hops', self.attr.hops));
 
-                    title.text(pathsFoundText).closest('.popover-title').show();
-                    self.positionDialog();
-                });
+                    //title.text(pathsFoundText).closest('.popover-title').show();
+                    //self.positionDialog();
+                //});
         }
 
         this.findPath = function(source, dest) {
@@ -127,26 +127,27 @@ define([
                 sourceGraphVertexId: source,
                 destGraphVertexId: dest,
                 depth: 5,
-                hops: this.attr.hops
+                // TODO: fix
+                hops: 4 //this.attr.hops
             };
 
             return this.vertexService.findPath(parameters)
-                        .then(function(data) {
-                            var vertices = [], added = {};
-                            data.paths.forEach(function(path) {
-                                path.forEach(function(vertex) {
-                                    if (!added[vertex.id]) {
-                                        vertices.push(vertex);
-                                        added[vertex.id] = true;
-                                    }
-                                });
-                            });
+                        //.then(function(data) {
+                            //var vertices = [], added = {};
+                            //data.paths.forEach(function(path) {
+                                //path.forEach(function(vertex) {
+                                    //if (!added[vertex.id]) {
+                                        //vertices.push(vertex);
+                                        //added[vertex.id] = true;
+                                    //}
+                                //});
+                            //});
 
-                            return {
-                                paths: data.paths,
-                                uniqueVertices: vertices
-                            };
-                        });
+                            //return {
+                                //paths: data.paths,
+                                //uniqueVertices: vertices
+                            //};
+                        //});
         };
 
         this.onFindPathButton = function(e) {
