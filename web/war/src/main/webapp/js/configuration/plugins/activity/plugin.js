b83e8adb77a7b40f2286f428d0d50ce849be0e50
Jason Harwig <jason@pinepointsoftware.com>
2014-10-28 14:01:53 -0500

    Add activity badge showing current number of tasks

--- web/war/src/main/webapp/js/configuration/plugins/activity/plugin.js
+++ web/war/src/main/webapp/js/configuration/plugins/activity/plugin.js
@@ -1,16 +1,28 @@
-define([], function() {
+define(['jquery'], function($) {
     'use strict';
 
     var activityHandlers = [],
         byType = {},
+        byKind = {},
+        triggerUpdate = _.throttle(function() {
+            $(document).trigger('activityHandlersUpdated');
+        }, 2000),
         api = {
             activityHandlers: activityHandlers,
 
             activityHandlersByType: byType,
 
+            activityHandlersByKind: byKind,
+
             registerActivityHandler: function reg(handler) {
                 activityHandlers.push(handler);
                 byType[handler.type] = handler;
+                if (!(handler.kind in byKind)) {
+                    byKind[handler.kind] = [handler];
+                } else {
+                    byKind[handler.kind].push(handler);
+                }
+                triggerUpdate();
             },
 
             registerActivityHandlers: function(handlers) {
