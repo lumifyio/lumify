dff38a3cdcf6946c9f2c29a313b59cde40dec2f4
Joe Ferner <joe@fernsroth.com>
2014-11-07 10:31:12 -0500

    change to using the new addEdge using vertex ids

--- datasets/palantir/data-import/src/main/java/io/lumify/palantir/dataImport/PtObjectObjectImporter.java
+++ datasets/palantir/data-import/src/main/java/io/lumify/palantir/dataImport/PtObjectObjectImporter.java
@@ -1,18 +1,13 @@
 package io.lumify.palantir.dataImport;
 
-import com.google.common.base.Function;
-import com.google.common.collect.Iterables;
 import io.lumify.core.exception.LumifyException;
 import io.lumify.palantir.dataImport.model.PtLinkType;
 import io.lumify.palantir.dataImport.model.PtObjectObject;
 import org.securegraph.Vertex;
 
-import java.util.List;
-import java.util.Map;
-
 import static com.google.common.base.Preconditions.checkNotNull;
 
-public class PtObjectObjectImporter extends PtGroupingImporterBase<PtObjectObject, Long> {
+public class PtObjectObjectImporter extends PtRowImporterBase<PtObjectObject> {
     protected PtObjectObjectImporter(final DataImporter dataImporter) {
         super(dataImporter, PtObjectObject.class);
     }
@@ -24,38 +19,18 @@ public class PtObjectObjectImporter extends PtGroupingImporterBase<PtObjectObjec
     }
 
     @Override
-    protected void processGroup(Long parentObjectId, List<PtObjectObject> rows) throws Exception {
-        String sourceObjectId = getObjectVertexId(parentObjectId);
-        Vertex sourceVertex = getVertexCache().get(sourceObjectId);
-        checkNotNull(sourceVertex, "Could not find source vertex: " + sourceObjectId);
+    protected void processRow(PtObjectObject row) throws Exception {
+        String sourceVertexId = getObjectVertexId(row.getParentObjectId());
+        String destVertexId = getObjectVertexId(row.getChildObjectId());
 
-        Iterable<String> destVertexIds = Iterables.transform(rows, new Function<PtObjectObject, String>() {
-            @Override
-            public String apply(PtObjectObject row) {
-                return getObjectVertexId(row.getChildObjectId());
-            }
-        });
-        Map<String, Vertex> destVertices = getVertexCacheVertices(destVertexIds);
-
-        for (PtObjectObject row : rows) {
-            PtLinkType ptLinkType = getDataImporter().getLinkTypes().get(row.getType());
-            if (ptLinkType == null) {
-                throw new LumifyException("Could not find link type: " + row.getType());
-            }
-            String linkTypeUri = getLinkTypeUri(ptLinkType.getUri());
-
-            String destObjectId = getObjectVertexId(row.getChildObjectId());
-            Vertex destVertex = destVertices.get(destObjectId);
-            checkNotNull(destVertex, "Could not find dest vertex: " + destObjectId);
-
-            String edgeId = getEdgeId(row);
-            getDataImporter().getGraph().addEdge(edgeId, sourceVertex, destVertex, linkTypeUri, getDataImporter().getVisibility(), getDataImporter().getAuthorizations());
+        PtLinkType ptLinkType = getDataImporter().getLinkTypes().get(row.getType());
+        if (ptLinkType == null) {
+            throw new LumifyException("Could not find link type: " + row.getType());
         }
-    }
+        String linkTypeUri = getLinkTypeUri(ptLinkType.getUri());
 
-    @Override
-    protected Long getGroupKey(PtObjectObject ptObjectObject) {
-        return ptObjectObject.getParentObjectId();
+        String edgeId = getEdgeId(row);
+        getDataImporter().getGraph().addEdge(edgeId, sourceVertexId, destVertexId, linkTypeUri, getDataImporter().getVisibility(), getDataImporter().getAuthorizations());
     }
 
     protected String getEdgeId(PtObjectObject ptObjectObject) {
