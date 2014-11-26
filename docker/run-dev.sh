05a3e86bbef827e59364dcc7be7b79671195f454
Joe Ferner <joe@fernsroth.com>
2014-11-26 13:09:59 -0500

    document ports

--- docker/run-dev.sh
+++ docker/run-dev.sh
@@ -19,21 +19,25 @@ sudo docker run \
   -v ${FS_DIR}/var/local/hadoop:/var/local/hadoop \
   -v ${FS_DIR}/opt/elasticsearch-1.4.0/data:/opt/elasticsearch-1.4.0/data \
   -v ${FS_DIR}/opt/rabbitmq_server-3.4.1/var:/opt/rabbitmq_server-3.4.1/var \
-  -p 2181:2181 \
-  -p 9200:9200 \
-  -p 9300:9300 \
-  -p 9997:9997 \
-  -p 9999:9999 \
-  -p 50010:50010 \
-  -p 50020:50020 \
-  -p 50030:50030 \
-  -p 50070:50070 \
-  -p 50075:50075 \
-  -p 50090:50090 \
-  -p 50095:50095 \
-  -p 5672:5672 \
-  -p 5673:5673 \
-  -p 15672:15672 \
+  -p 2181:2181 `# ZooKeeper` \
+  -p 5672:5672 `# RabbitMQ` \
+  -p 5673:5673 `# RabbitMQ` \
+  -p 8032:8032 `# Hadoop: Resource Manager` \
+  -p 8088:8088 `# Hadoop: Resource Manager: Web UI` \
+  -p 9000:9000 `# Hadoop: Name Node: Metadata Service` \
+  -p 9200:9200 `# Elasticsearch` \
+  -p 9300:9300 `# Elasticsearch` \
+  -p 9997:9997 `# Accumulo` \
+  -p 9999:9999 `# Accumulo` \
+  -p 15672:15672 `# RabbitMQ: Web UI` \
+  -p 50010:50010 `# Hadoop: Data Node: Data transfer` \
+  -p 50020:50020 `# Hadoop: Data Node: Metadata operations` \
+  -p 50030:50030 `# Hadoop: Job Tracker` \
+  -p 50060:50060 `# Hadoop: Node Manager: Web UI` \
+  -p 50070:50070 `# Hadoop: Name Node: Web UI` \
+  -p 50075:50075 `# Hadoop: Data Node: Web UI` \
+  -p 50090:50090 `# Hadoop: Secondary Name Node` \
+  -p 50095:50095 `# Accumulo: Web UI` \
   -i \
   -t \
   -h lumify-dev \
