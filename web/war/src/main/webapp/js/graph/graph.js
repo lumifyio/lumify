54f91fbdb1790c9c1818ffa5e38776dc0a825fc9
Jason Harwig <jason@pinepointsoftware.com>
2015-01-02 18:04:18 -0600

    Initial graph options to toggle edge labels, needs extension point

--- web/war/src/main/webapp/js/graph/graph.js
+++ web/war/src/main/webapp/js/graph/graph.js
@@ -1606,7 +1606,12 @@ define([
 
                     self.bindContextMenuClickEvent();
 
-                    Controls.attachTo(self.select('graphToolsSelector'));
+                    Controls.attachTo(self.select('graphToolsSelector'), {
+                        optionsComponentPath: 'graph/options',
+                        optionsAttributes: {
+                            cy: self.cytoscapeReady()
+                        }
+                    });
 
                     var $views = $();
                     self.updateGraphViewsPosition();
