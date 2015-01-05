24bc5c8ff4015c12ce553561ff07ad69d64613b8
Joe Ferner <joe@fernsroth.com>
2015-01-05 15:47:28 -0500

    remove joda dependency

--- web/client-api/src/main/java/io/lumify/web/clientapi/model/util/ObjectMapperFactory.java
+++ web/client-api/src/main/java/io/lumify/web/clientapi/model/util/ObjectMapperFactory.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.joda.JodaModule;
 
 public class ObjectMapperFactory {
     private static ObjectMapper mapper;
@@ -15,7 +14,6 @@ public class ObjectMapperFactory {
             mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
             mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
             mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-            mapper.registerModule(new JodaModule());
         }
         return mapper;
     }
