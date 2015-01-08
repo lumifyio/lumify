c91a100d8f13f8448560ad8d8f4206ae61abac92
Jason Harwig <jason@pinepointsoftware.com>
2015-01-08 12:44:24 -0600

    Style edge list

--- web/war/src/main/webapp/js/util/edge/list.js
+++ web/war/src/main/webapp/js/util/edge/list.js
@@ -13,14 +13,19 @@ define([
         this.after('initialize', function() {
             console.log(this.attr)
             d3.select(this.node)
+                .classed({
+                    'vertices-list': true,
+                    'vertex-list': true
+                })
                 .append('ul').attr('class', 'nav nav-list')
                 .selectAll('li.edge')
                 .data(this.attr.edges)
                 .call(function() {
                     this.enter()
                         .append('li').attr('class', 'edge')
+                        .append('a')
 
-                    this.text(function(d) {
+                    this.select('a').text(function(d) {
                         return d.properties[0].value.justificationText;
                     });
 
