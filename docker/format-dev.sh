838ad59f3ebe93bae46678be6dcc022b372f4e79
Joe Ferner <joe@fernsroth.com>
2015-01-14 14:29:30 -0500

    change format to work on windows

--- docker/format-dev.sh
+++ docker/format-dev.sh
@@ -1,12 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do
-  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
-done
-DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+DIR=$(cd $(dirname "$0") && pwd)
 
 SRC_DIR=${DIR}/..
 
@@ -18,6 +12,8 @@ case $(uname) in
   Darwin)
     dev=$(boot2docker ssh blkid -L boot2docker-data)
     mnt=$(echo "$(boot2docker ssh mount)" | awk -v dev=${dev} '$1 == dev && !seen {print $3; seen = 1}')
+    uid=$(boot2docker ssh id -u)
+    gid=$(boot2docker ssh id -g)
     PERSISTENT_DIR=${mnt}/lumify-dev-persistent
     boot2docker ssh sudo rm -rf ${PERSISTENT_DIR}
     ;;
