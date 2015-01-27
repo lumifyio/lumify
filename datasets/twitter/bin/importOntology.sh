8ad282c358a325042a0be62088e315a68550e614
zirafel <raffaele.palmieri@gmail.com>
2015-01-27 15:45:59 +0100

    fix classpath import twitter ontology #528

--- datasets/twitter/bin/importOntology.sh
+++ datasets/twitter/bin/importOntology.sh
@@ -8,7 +8,7 @@ while [ -h "$SOURCE" ]; do
 done
 DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 
-classpath=$(${DIR}/classpath.sh .)
+classpath=$(${DIR}/classpath.sh "twitter-graph-property-worker")
 if [ $? -ne 0 ]; then
   echo "${classpath}"
   exit
