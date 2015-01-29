774d0ad8f25478a35a861a254a5995072259e5e5
David Singley <dsingley@nearinfinity.com>
2015-01-29 16:07:44 -0500

    add --boot2docker option to docker dev format script

--- docker/format-dev.sh
+++ docker/format-dev.sh
@@ -2,23 +2,16 @@
 
 DIR=$(cd $(dirname "$0") && pwd)
 
-case $(uname) in
-  Linux)
-    PERSISTENT_DIR=${DIR}/lumify-dev-persistent
-    rm -rf ${PERSISTENT_DIR}
-    ;;
-  Darwin)
-    dev=$(boot2docker ssh blkid -L boot2docker-data)
-    mnt=$(echo "$(boot2docker ssh mount)" | awk -v dev=${dev} '$1 == dev && !seen {print $3; seen = 1}')
-    uid=$(boot2docker ssh id -u)
-    gid=$(boot2docker ssh id -g)
-    PERSISTENT_DIR=${mnt}/lumify-dev-persistent
-    boot2docker ssh sudo rm -rf ${PERSISTENT_DIR}
-    LOCAL_PERSISTENT_DIR=${DIR}/lumify-dev-persistent
-    rm -rf ${LOCAL_PERSISTENT_DIR}
-    ;;
-  *)
-    echo "unexpected uname: $(uname)"
-    exit -1
-    ;;
-esac
+if [ $(uname) = 'Darwin' -o "$1" = '--boot2docker' ]; then
+  dev=$(boot2docker ssh blkid -L boot2docker-data)
+  mnt=$(echo "$(boot2docker ssh mount)" | awk -v dev=${dev} '$1 == dev && !seen {print $3; seen = 1}')
+  uid=$(boot2docker ssh id -u)
+  gid=$(boot2docker ssh id -g)
+  PERSISTENT_DIR=${mnt}/lumify-dev-persistent
+  boot2docker ssh sudo rm -rf ${PERSISTENT_DIR}
+  LOCAL_PERSISTENT_DIR=${DIR}/lumify-dev-persistent
+  rm -rf ${LOCAL_PERSISTENT_DIR}
+else
+  PERSISTENT_DIR=${DIR}/lumify-dev-persistent
+  rm -rf ${PERSISTENT_DIR}
+fi
