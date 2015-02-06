caabba960a437506702dd1048d28eeed538ef478
Jason Harwig <jason@pinepointsoftware.com>
2015-02-06 11:13:43 -0600

    Fix username-only button disabling

--- web/plugins/auth-username-only/src/main/resources/username-only/authentication.js
+++ web/plugins/auth-username-only/src/main/resources/username-only/authentication.js
@@ -80,8 +80,8 @@ define([
                 return;
             }
 
-            this.disabled = true;
             this.enableButton(false, true);
+            this.disabled = true;
             $error.empty();
 
             $.post('login', { username: $username.val() })
