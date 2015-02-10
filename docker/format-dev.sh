ab1724687b7e4954895ac33e4974cff35624556e
David Singley <dsingley@nearinfinity.com>
2015-02-10 11:11:33 -0500

    fix --boot2docker option

--- docker/format-dev.sh
+++ docker/format-dev.sh
@@ -2,14 +2,19 @@
 
 DIR=$(cd $(dirname "$0") && pwd)
 
-which boot2docker > /dev/null
-if[ $? -eq 0 ]; then
-  BOOT2DOCKER_SSH='boot2docker ssh'
-else
-  BOOT2DOCKER_SSH=
+if [ "$1" = '--boot2docker' ]; then
+  shift
+  SPLIT_PERSISTENT_DIR='true'
+
+  which boot2docker > /dev/null
+  if [ $? -eq 0 ]; then
+    BOOT2DOCKER_SSH='boot2docker ssh'
+  else
+    BOOT2DOCKER_SSH=
+  fi
 fi
 
-if [ $(uname) = 'Darwin' -o "$1" = '--boot2docker' ]; then
+if [ $(uname) = 'Darwin' -o "${SPLIT_PERSISTENT_DIR}" = 'true' ]; then
   dev=$(${BOOT2DOCKER_SSH} blkid -L boot2docker-data)
   mnt=$(echo "$(${BOOT2DOCKER_SSH} mount)" | awk -v dev=${dev} '$1 == dev && !seen {print $3; seen = 1}')
   uid=$(${BOOT2DOCKER_SSH} id -u)
