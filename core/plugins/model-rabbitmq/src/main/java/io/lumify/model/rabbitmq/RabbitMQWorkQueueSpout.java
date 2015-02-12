8831127a899f0a6604293bb61986b556b43629fd
Joe Ferner <joe@fernsroth.com>
2015-02-12 14:01:41 -0500

    set default rabbitmq prefetch count to 10

--- core/plugins/model-rabbitmq/src/main/java/io/lumify/model/rabbitmq/RabbitMQWorkQueueSpout.java
+++ core/plugins/model-rabbitmq/src/main/java/io/lumify/model/rabbitmq/RabbitMQWorkQueueSpout.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 
 public class RabbitMQWorkQueueSpout extends WorkerSpout {
     private static final LumifyLogger LOGGER = LumifyLoggerFactory.getLogger(RabbitMQWorkQueueSpout.class);
+    public static final int DEFAULT_RABBITMQ_PREFETCH_COUNT = 10;
     private final String queueName;
     private Channel channel;
     private QueueingConsumer consumer;
@@ -35,7 +36,7 @@ public class RabbitMQWorkQueueSpout extends WorkerSpout {
             this.channel.queueDeclare(queueName, true, false, false, null);
             this.consumer = new QueueingConsumer(channel);
             this.channel.basicConsume(this.queueName, false, consumer);
-            this.channel.basicQos(configuration.getInt(Configuration.RABBITMQ_PREFETCH_COUNT, 0));
+            this.channel.basicQos(configuration.getInt(Configuration.RABBITMQ_PREFETCH_COUNT, DEFAULT_RABBITMQ_PREFETCH_COUNT));
         } catch (IOException ex) {
             throw new LumifyException("Could not startup RabbitMQ", ex);
         }
