7998d0322f818d83bc20f8df3e3614a4a6cdcc6b
Thomas Hampton <thampton@solutionstreet.com>
2015-04-28 16:53:41 -0400

    LUM-2 to make the build faster, download the prepared clavin indexes instead of building them

--- docker/scripts/install-clavin.sh
+++ docker/scripts/install-clavin.sh
@@ -8,12 +8,18 @@ mkdir ${index_dir}
 mkdir /opt/clavin-${clavin_version}
 cd /opt/clavin-${clavin_version}
 curl -L -o /opt/clavin-${clavin_version}/${clavin_jar} -O https://bits.lumify.io/extra/${clavin_jar}
-curl -L -o /opt/clavin-${clavin_version}/allCountries.zip -O https://bits.lumify.io/extra/allCountries.zip
-curl -L -o /opt/clavin-${clavin_version}/alternateNames.zip -O https://bits.lumify.io/extra/alternateNames.zip
-unzip allCountries.zip
-unzip alternateNames.zip
 
-jar xf clavin-${clavin_version}-jar-with-dependencies.jar SupplementaryGazetteer.txt
-java -Xmx3g -cp clavin-${clavin_version}-jar-with-dependencies.jar com.bericotech.clavin.index.IndexDirectoryBuilder -r -o ${index_dir} -i allCountries.txt:SupplementaryGazetteer.txt --alt-names-file alternateNames.txt
+#
+# These steps will generate the index, but we'll download it instead
+#
+#curl -L -o /opt/clavin-${clavin_version}/allCountries.zip -O https://bits.lumify.io/extra/allCountries.zip
+#curl -L -o /opt/clavin-${clavin_version}/alternateNames.zip -O https://bits.lumify.io/extra/alternateNames.zip
+#unzip allCountries.zip
+#unzip alternateNames.zip
+#jar xf clavin-${clavin_version}-jar-with-dependencies.jar SupplementaryGazetteer.txt
+#java -Xmx3g -cp clavin-${clavin_version}-jar-with-dependencies.jar com.bericotech.clavin.index.IndexDirectoryBuilder -r -o ${index_dir} -i allCountries.txt:SupplementaryGazetteer.txt --alt-names-file alternateNames.txt
+
+curl -L -o /opt/clavin-index-2.0.0_2015-04-28.tar.gz -O https://bits.lumify.io/data/clavin-index-2.0.0_2015-04-28.tar.gz
+tar -zxvf /opt/clavin-index-2.0.0_2015-04-28.tar.gz -C /opt
 
 rm *.txt *.zip
