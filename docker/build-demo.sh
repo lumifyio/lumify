c9f07f4749fd75792cb3131fb4d2ca6ec494a775
Thomas Hampton <thampton@solutionstreet.com>
2015-04-30 09:36:24 -0400

    LUM-2 Added building lumify to the docker build

--- docker/build-demo.sh
+++ docker/build-demo.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 DIR=$(cd $(dirname "$0") && pwd)
-cd ${DIR}
-
 SRC_DIR=${DIR}/..
 
-mkdir -p demo/tmp
+cd $SRC_DIR
+mvn -P "grunt unix",web-war,web-war-with-gpw,web-war-with-ui-plugins clean package -DskipTests
 
-cp ${SRC_DIR}/web/war/target/lumify-web-war-*.war demo/tmp/root.war
-# cp ${SRC_DIR}/web/plugins/auth-username-only/target/lumify-web-auth-username-only-*.jar demo/tmp
-cp ${SRC_DIR}/config/log4j.xml demo/tmp
+cd $DIR
+mkdir -p demo/.tmp
+cp ${SRC_DIR}/web/war/target/lumify-web-war-*.war demo/.tmp/root.war
+cp ${SRC_DIR}/config/log4j.xml demo/.tmp
+# cp ${SRC_DIR}/web/plugins/auth-username-only/target/lumify-web-auth-username-only-*.jar demo/.tmp
 
 case $(uname) in
   Linux)
