72015bed077eaa1aa104e635afc328ceddb8516f
Thomas Hampton <thampton@solutionstreet.com>
2015-05-04 11:23:04 -0400

    LUM-2 script to deploy Lumify to a running docker container

---
+++ docker/demo/deploy-lumify.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#
+# This script will deploy Lumify to a running Docker container
+#
+
+DIR=$(cd $(dirname "$0") && pwd)
+SRC_DIR=$DIR/../..
+
+SSH_KEY=$DIR/keys/id_rsa
+SSH_OPTIONS="-p 2022 -i $SSH_KEY -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+SCP_OPTIONS="-P 2022 -i $SSH_KEY -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+
+# stop Jetty
+ssh $SSH_OPTIONS root@lumify-demo '/opt/jetty/bin/jetty.sh stop'
+
+# deploy Lumify artifacts
+scp $SCP_OPTIONS    $SRC_DIR/config/log4j.xml                     root@lumify-demo:/opt/lumify/config/log4j.xml
+scp $SCP_OPTIONS    $SRC_DIR/docker/demo/lumify.properties        root@lumify-demo:/opt/lumify/config/lumify.properties
+scp $SCP_OPTIONS    $SRC_DIR/web/war/target/lumify-web-war-*.war  root@lumify-demo:/opt/jetty/webapps/root.war
+scp $SCP_OPTIONS -r $SRC_DIR/examples/ontology-minimal            root@lumify-demo:/opt/lumify/ontology
+
+# start Jetty
+ssh $SSH_OPTIONS root@lumify-demo '/opt/jetty/bin/jetty.sh start </dev/null >/dev/null 2>&1 &'
+
+exit 0
