7a412f0e1b31569b27158de1ee4dd3705bdf8173
Leon Pham <LeonP92@vt.edu>
2015-07-06 15:51:47 -0400

    Updated JAI URLs.

--- vagrant/scripts/install-java.sh
+++ vagrant/scripts/install-java.sh
@@ -22,22 +22,22 @@ rm -rf $ARCHIVE_DIR
 # build the package
 ln -s /opt/jdk1.7.0_71 /opt/jdk
 
-JAI_VERSION=1.1.3
-JAI_IMAGEIO_VERSION=1.1
+JAI_FILE=jai-1_1_3-lib-linux-amd64-jdk.bin
+JAI_IMAGEIO_FILE=jai_imageio-1_1-lib-linux-amd64-jdk.bin
 
 (cd /opt/jdk
-  curl -L http://download.java.net/media/jai/builds/release/$(echo ${JAI_VERSION} | sed -e 's/\./_/g')/jai-$(echo ${JAI_VERSION} | sed -e 's/\./_/g')-lib-linux-amd64-jdk.bin -O
+  curl -L https://bits.lumify.io/extra/java/${JAI_FILE} -O
   sed -i -e 's|more <<EOF|cat > /dev/null <<EOF|' \
          -e 's/agreed=$/agreed=1/' \
-         jai-$(echo ${JAI_VERSION} | sed -e 's/\./_/g')-lib-linux-amd64-jdk.bin
-  chmod u+x jai-$(echo ${JAI_VERSION} | sed -e 's/\./_/g')-lib-linux-amd64-jdk.bin
-  ./jai-$(echo ${JAI_VERSION} | sed -e 's/\./_/g')-lib-linux-amd64-jdk.bin && rm ./jai-$(echo ${JAI_VERSION} | sed -e 's/\./_/g')-lib-linux-amd64-jdk.bin
+         ${JAI_FILE}
+  chmod u+x ${JAI_FILE}
+  ./${JAI_FILE} && rm ./${JAI_FILE}
 
-  curl -L http://download.java.net/media/jai-imageio/builds/release/${JAI_IMAGEIO_VERSION}/jai_imageio-$(echo ${JAI_IMAGEIO_VERSION} | sed -e 's/\./_/g')-lib-linux-amd64-jdk.bin -O
-  chmod u+x jai_imageio-$(echo ${JAI_IMAGEIO_VERSION} | sed -e 's/\./_/g')-lib-linux-amd64-jdk.bin
+  curl -L https://bits.lumify.io/extra/java/${JAI_IMAGEIO_FILE} -O
+  chmod u+x ${JAI_IMAGEIO_FILE}
   sed -i -e 's|more <<EOF|cat > /dev/null <<EOF|' \
          -e 's/agreed=$/agreed=1/' \
          -e 's/^tail +/tail -n +/' \
-         jai_imageio-$(echo ${JAI_IMAGEIO_VERSION} | sed -e 's/\./_/g')-lib-linux-amd64-jdk.bin
-  ./jai_imageio-$(echo ${JAI_IMAGEIO_VERSION} | sed -e 's/\./_/g')-lib-linux-amd64-jdk.bin && rm jai_imageio-$(echo ${JAI_IMAGEIO_VERSION} | sed -e 's/\./_/g')-lib-linux-amd64-jdk.bin
+         ${JAI_IMAGEIO_FILE}
+  ./${JAI_IMAGEIO_FILE} && rm ./${JAI_IMAGEIO_FILE}
 )
